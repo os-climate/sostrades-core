da5c5b528c733a368a9a71aec45e65ae5cde301e
CARLIN DE CLAIR @AIRBUS <carlin.tchuisseu-fongang.external@airbus.com>
2022-02-17 15:27:04 +0100

    git script version 3
    Signed-off-by: CARLIN DE CLAIR @AIRBUS <carlin.tchuisseu-fongang.external@airbus.com>

--- git_script.py
+++ git_script.py
@@ -13,7 +13,7 @@ def update_os_climate(repos_list, branches_list):
             except:
                 print("upstream branch not present in osclimate, branch created")
             git.push('osclimate', branch)
-    print("done 2 ")
+    print("done 3 ")
 
 
 if '__main__' == __name__:
