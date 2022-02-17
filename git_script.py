216a02550d965b546c3ee4b76a7d7836a8a86fe7
carlin <carlin.tchuisseu-fongang.external@airbus.com>
2022-02-17 15:14:11 +0100

    git script version1

--- git_script.py
+++ git_script.py
@@ -1,37 +1,20 @@
 from git import Repo
 
-def update_os_climate(repos_list,branches_list):
+
+def update_os_climate(repos_list, branches_list):
     for repository in repos_list:
         repo = Repo(repository)
-        git  = repo.git
-        git.pull()
+        git = repo.git
         for branch in branches_list:
+            git.pull('origin', branch)
             git.checkout(branch)
-            git.pull('osclimate',branch,'--allow-unrelated-histories')
-            git.push('osclimate',branch)
+            try:
+                git.pull('osclimate', branch, '--allow-unrelated-histories')
+            except:
+                print("upstream branch not present in osclimate, branch created")
+            git.push('osclimate', branch)
     print("done")
 
 
-
 if '__main__' == __name__:
-    update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'],['develop','integration'])
-    # #repo = Repo(r'C:\Users\SP005188\eclipse-workspace\SoSTrades\witness-energy')
-    # repo = Repo()
-    # print(repo.working_dir)
-    # git = repo.git
-    # #print(git.status())
-    # #print(git.remote('-v'))
-    # print(git.pull())
-    # #print(git.push('os'))
-    # #print(git.status())
-    # #print(git.status('origin',C=r'C:\Users\SP005188\eclipse-workspace\SoSTrades\witness-energy' ))
-    # print(git.checkout('develop'))
-    # print(git.pull('osclimate','develop','--allow-unrelated-histories'))
-    # #print(git.commit('--amend','--signoff'))
-    # print(git.push('osclimate','develop'))
-    #
-    # #cloned_repo = Repo.clone_from('https://idas661.eu.airbus.corp/sostrades/sostrades-core.git',r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core\git_test')
-    # #assert cloned_repo.__class__ is Repo
-    # print('a simple change')
-
-
+    update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
