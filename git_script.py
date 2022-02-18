c1bf24e501e68cab05e49a74cd4fc251b515c882
CARLIN DE CLAIR @AIRBUS <SP005188@eu.airbus.corp>
2022-02-18 11:53:47 +0100

    git script final version
    Signed-off-by: CARLIN DE CLAIR @AIRBUS <SP005188@eu.airbus.corp>

--- git_script.py
+++ git_script.py
@@ -8,19 +8,15 @@ def update_os_climate(repos_list, branches_list):
         for branch in branches_list:
             git.checkout(branch)
             git.pull('origin', branch)
-#            try:
-#                git.pull('osclimate', branch, '--allow-unrelated-histories')
-#            except:
-#                print("upstream branch not present in osclimate, branch created")
             try:
                 print(git.push('osclimate', branch))
             except:
-                msg = "Something went wrong during the merge of branch %s "%branch
-                msg += "of repository %s" %repository
+                msg = "Something went wrong during the merge of branch %s " % branch
+                msg += "of repository %s" % repository
                 print(msg)
                 raise
 
 
 if '__main__' == __name__:
-    update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
-    print("update done")
+    pass
+    # update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
