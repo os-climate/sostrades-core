dc2120d74f4cc0a5e7085ae1704c4e8be472b580
CARLIN DE CLAIR @AIRBUS <SP005188@eu.airbus.corp>
2022-02-18 11:50:32 +0100

    testing PJ modifications on script
    Signed-off-by: CARLIN DE CLAIR @AIRBUS <SP005188@eu.airbus.corp>

--- git_script.py
+++ git_script.py
@@ -6,14 +6,21 @@ def update_os_climate(repos_list, branches_list):
         repo = Repo(repository)
         git = repo.git
         for branch in branches_list:
-            git.pull('origin', branch)
             git.checkout(branch)
+            git.pull('origin', branch)
+#            try:
+#                git.pull('osclimate', branch, '--allow-unrelated-histories')
+#            except:
+#                print("upstream branch not present in osclimate, branch created")
             try:
-                git.pull('osclimate', branch, '--allow-unrelated-histories')
+                print(git.push('osclimate', branch))
             except:
-                print("upstream branch not present in osclimate, branch created")
-            git.push('osclimate', branch)
+                msg = "Something went wrong during the merge of branch %s "%branch
+                msg += "of repository %s" %repository
+                print(msg)
+                raise
 
 
 if '__main__' == __name__:
     update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
+    print("update done")
