6b1378708a21f2c74c583f05761f08b3dc8c901e
CARLIN DE CLAIR @AIRBUS <SP005188@eu.airbus.corp>
2022-02-21 09:34:21 +0100

    adding signed push

--- sos_trades_core/tools/git/git_script.py
+++ sos_trades_core/tools/git/git_script.py
@@ -9,7 +9,7 @@ def update_os_climate(repos_list, branches_list):
             git.checkout(branch)
             git.pull('origin', branch)
             try:
-                print(git.push('osclimate', branch))
+                print(git.push('osclimate', branch,'--signed=if-asked'))
             except:
                 msg = "Something went wrong during the merge of branch %s " % branch
                 msg += "of repository %s" % repository
@@ -18,5 +18,5 @@ def update_os_climate(repos_list, branches_list):
 
 
 if '__main__' == __name__:
-    pass
-    # update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
+    #pass
+    update_os_climate([r'C:\Users\SP005188\eclipse-workspace\SoSTrades\sostrades-core'], ['develop'])
