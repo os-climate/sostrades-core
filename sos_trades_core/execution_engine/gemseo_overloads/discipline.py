3c6740fb3873d4706720bac7ce69c69cf0f44756
LEONARD Elise <NG878B4@eu.airbus.corp>
2022-02-21 18:14:08 +0100

    add comments

--- sos_trades_core/execution_engine/gemseo_overloads/discipline.py
+++ sos_trades_core/execution_engine/gemseo_overloads/discipline.py
@@ -15,9 +15,6 @@ limitations under the License.
 '''
 from timeit import default_timer as timer
 from gemseo.core.discipline import MDODiscipline
-from sos_trades_core.execution_engine.sos_discipline import SoSDiscipline
-from gemseo.core.chain import MDOChain
-from gemseo.mda.gauss_seidel import MDAGaussSeidel
 
 
 def execute(
