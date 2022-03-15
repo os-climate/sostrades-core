b9f1254e898eea125e7e122b1e0a3eb825ff0e6d
Albane SOFIA-GABRION <albane.sofia-gabrion.external@airbus.com>
2022-03-15 11:54:15 +0100

    graph plot grid search

--- sos_trades_core/sos_wrapping/analysis_discs/grid_search_eval.py
+++ sos_trades_core/sos_wrapping/analysis_discs/grid_search_eval.py
@@ -690,8 +690,6 @@ class GridSearchEval(DoeEval):
             doe_samples_df = outputs_dict['doe_samples_dataframe']
             cont_plot_df = doe_samples_df.merge(
                 output_df, how="left", on='scenario')
-            cont_plot_df.replace('NA', np.nan, inplace=True)
-            
 
             # we go through the list of charts and draw all of them
             for name, chart_info in chart_dict.items():
@@ -808,6 +806,7 @@ class GridSearchEval(DoeEval):
                         z_min = chart_info['z_min']
 
                         fig = go.Figure()
+                        z_data_None = []
 
                         for slide_value in slider_values:
                             x_data = cont_plot_df.loc[
@@ -819,6 +818,10 @@ class GridSearchEval(DoeEval):
                             z_data = cont_plot_df.loc[
                                 cont_plot_df[col_slider] == slide_value
                             ][chart_info['z']].replace(np.nan, 'None').to_list()
+                            if all(z_data[i] == 'None' for i in range(len(z_data))):
+                                z_data_None.append(True)
+                            else:
+                                z_data_None.append(False)
                             labels = cont_plot_df.loc[cont_plot_df[col_slider]
                                                       == slide_value]['scenario']
 
@@ -952,12 +955,11 @@ class GridSearchEval(DoeEval):
                         # Create native plotly chart
                         last_value = slider_values[-1]
                         if len(fig.data) > 0:
-                            if not all(list(fig.data[0]['z'])[i]=='None' for i in range(len(fig.data[0]['z']))):
+                            if not any(z_data_None):
                                 chart_name = f'<b>{name}</b>'
                                 new_chart = InstantiatedPlotlyNativeChart(
                                     fig=fig, chart_name=chart_name, default_legend=False
                                 )
                                 instanciated_charts.append(new_chart)
-                            
 
         return instanciated_charts
