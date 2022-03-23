ef2312fbb6d9d54dbde0f848e3ef7798c97f6e19
Albane SOFIA-GABRION <albane.sofia-gabrion.external@airbus.com>
2022-03-23 16:02:29 +0100

    modif grid search l1 test

--- sos_trades_core/sos_wrapping/analysis_discs/grid_search_eval.py
+++ sos_trades_core/sos_wrapping/analysis_discs/grid_search_eval.py
@@ -127,14 +127,14 @@ class GridSearchEval(DoeEval):
 
             if set(selected_inputs.tolist()) != set(self.selected_inputs):
                 selected_inputs_has_changed = True
-                self.selected_inputs = selected_inputs.tolist()
-                self.dm.set_data(
-                    f'{self.get_disc_full_name()}.eval_inputs',
-                    'value',
-                    eval_inputs.sort_values(
-                        by=['selected_input', 'full_name'], ascending=False).reset_index(drop=True),
-                    check_value=False,
-                )
+                # self.selected_inputs = selected_inputs.tolist()
+                # self.dm.set_data(
+                #     f'{self.get_disc_full_name()}.eval_inputs',
+                #     'value',
+                #     eval_inputs.sort_values(
+                #         by=['selected_input', 'full_name'], ascending=False).reset_index(drop=True),
+                #     check_value=False,
+                # )
             if set(selected_outputs.tolist()) != set(self.selected_outputs):
                 self.dm.set_data(
                     f'{self.get_disc_full_name()}.eval_outputs',
@@ -144,6 +144,15 @@ class GridSearchEval(DoeEval):
                     check_value=False,
                 )
             self.selected_outputs = selected_outputs.tolist()
+
+            self.selected_inputs = selected_inputs.tolist()
+            self.dm.set_data(
+                f'{self.get_disc_full_name()}.eval_inputs',
+                'value',
+                eval_inputs.sort_values(
+                    by=['selected_input', 'full_name'], ascending=False).reset_index(drop=True),
+                check_value=False,
+            )
 
             selected_inputs_short = eval_inputs[eval_inputs['selected_input'] == True][
                 'shortest_name'
