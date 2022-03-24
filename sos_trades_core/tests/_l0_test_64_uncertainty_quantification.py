78d1a2e0d69a958e2e5b2b4979f735e807805d0e
Etienne Tomiak <etienne.e.tomiak.external@airbus.com>
2022-03-24 11:14:41 +0100

    correction of import to avoid pylint error

--- sos_trades_core/tests/_l0_test_64_uncertainty_quantification.py
+++ sos_trades_core/tests/_l0_test_64_uncertainty_quantification.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from sos_trades_core.sos_processes.test import test_uncertainty_quantification
+# from sos_trades_core.sos_processes.test import test_uncertainty_quantification
 '''
 mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
 '''
@@ -23,7 +23,9 @@ import numpy as np
 from pandas.testing import assert_frame_equal
 
 from sos_trades_core.execution_engine.execution_engine import ExecutionEngine
-from sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification import UncertaintyQuantification
+from sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification import (
+    UncertaintyQuantification,
+)
 from sos_trades_core.execution_engine.sos_coupling import SoSCoupling
 from os.path import join, dirname
 
@@ -34,15 +36,17 @@ class TestUncertaintyQuantification(unittest.TestCase):
     """
 
     def setUp(self):
-        '''
+        """
         Initialize third data needed for testing
-        '''
+        """
         self.name = 'Test'
         self.ee = ExecutionEngine(self.name)
         self.factory = self.ee.factory
         self.namespace = 'MyCase'
         self.repo = 'sos_trades_core.sos_processes.test'
-        self.mod_path = 'sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification'
+        self.mod_path = (
+            'sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification'
+        )
         self.base_path = 'sos_trades_core.sos_wrapping.test_discs'
         self.uncertainty_quantification = 'UncertaintyQuantification'
         self.proc_name = 'test_uncertainty_quantification'
@@ -51,20 +55,26 @@ class TestUncertaintyQuantification(unittest.TestCase):
     def test_01_uncertainty_quantification(self):
 
         repo = 'sos_trades_core.sos_processes.test'
-        mod_path = 'sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification'
+        mod_path = (
+            'sos_trades_core.sos_wrapping.analysis_discs.uncertainty_quantification'
+        )
 
         builder = self.factory.get_builder_from_process(
-            repo, "test_uncertainty_quantification")
+            repo, "test_uncertainty_quantification"
+        )
 
         self.ee.factory.set_builders_to_coupling_builder(builder)
 
-        ns_dict = {'ns_ac': f'{self.name}', 'ns_public': f'{self.name}',
-                   'ns_energy': f'{self.name}',
-                   'ns_coc_ac': f'{self.name}',
-                   'ns_dmc_ac': f'{self.name}',
-                   'ns_data_ac': f'{self.name}',
-                   'ns_coc': f'{self.name}',
-                   'ns_uncertainty_quantification': f'{self.name}.UncertaintyQuantification'}
+        ns_dict = {
+            'ns_ac': f'{self.name}',
+            'ns_public': f'{self.name}',
+            'ns_energy': f'{self.name}',
+            'ns_coc_ac': f'{self.name}',
+            'ns_dmc_ac': f'{self.name}',
+            'ns_data_ac': f'{self.name}',
+            'ns_coc': f'{self.name}',
+            'ns_uncertainty_quantification': f'{self.name}.UncertaintyQuantification',
+        }
 
         self.ee.ns_manager.add_ns_def(ns_dict)
 
@@ -72,14 +82,21 @@ class TestUncertaintyQuantification(unittest.TestCase):
 
         self.ee.display_treeview_nodes()
 
-        self.data_dir = join(dirname(dirname(
-            __file__)), 'sos_processes', 'test', 'test_uncertainty_quantification', 'data')
-        self.data_df = pd.read_csv(
-            join(self.data_dir, 'data_df_2.csv'))[['variable', 'npv', 'total_free_cash_flow', 'max_peak_exposure']]
+        self.data_dir = join(
+            dirname(dirname(__file__)),
+            'sos_processes',
+            'test',
+            'test_uncertainty_quantification',
+            'data',
+        )
+        self.data_df = pd.read_csv(join(self.data_dir, 'data_df_2.csv'))[
+            ['variable', 'npv', 'total_free_cash_flow', 'max_peak_exposure']
+        ]
         self.data_df = self.data_df.rename(columns={'variable': 'scenario'})
 
         self.doe_samples_dataframe = pd.read_csv(
-            join(self.data_dir, 'doe_samples_df_2.csv'))
+            join(self.data_dir, 'doe_samples_df_2.csv')
+        )
 
         # self.input_distribution_parameters_df = pd.read_csv(
         #     join(self.data_dir, 'input_distribution_parameters_df.csv'))
@@ -100,7 +117,8 @@ class TestUncertaintyQuantification(unittest.TestCase):
         self.ee.execute()
 
         uncertainty_quanti_disc = self.ee.dm.get_disciplines_with_name(
-            f'{self.name}.{self.uncertainty_quantification}')[0]
+            f'{self.name}.{self.uncertainty_quantification}'
+        )[0]
 
         uncertainty_quanti_disc_output = uncertainty_quanti_disc.get_sosdisc_outputs()
         out_df = uncertainty_quanti_disc_output['output_interpolated_values_df']
