bde0c2afbe744bc88b82405f97a0e7869e64a22d
Valentin JONCQUIERES <valentin.joncquieres.external@airbus.com>
2022-05-05 11:14:38 +0200

    Add check var data in desc_in for coupling variables and shared input variables

--- sos_trades_core/execution_engine/sos_discipline.py
+++ sos_trades_core/execution_engine/sos_discipline.py
@@ -98,6 +98,9 @@ class SoSDiscipline(MDODiscipline):
     VAR_NAME = 'var_name'
     VISIBLE = 'visible'
     CONNECTOR_DATA = 'connector_data'
+
+    DATA_TO_CHECK = [TYPE, UNIT, RANGE,
+                     POSSIBLE_VALUES, USER_LEVEL]
     # Dict  ex: {'ColumnName': (column_data_type, column_data_range,
     # column_editable)}
     DATAFRAME_DESCRIPTOR = 'dataframe_descriptor'
@@ -541,10 +544,11 @@ class SoSDiscipline(MDODiscipline):
                 # set cache_type and cache_file_path input values to children
                 for disc in self.sos_disciplines:
                     if 'cache_type' in disc._data_in:
-                        self.dm.set_data(disc.get_var_full_name('cache_type', disc._data_in), self.VALUE, cache_type,
-                                         check_value=False)
-                        self.dm.set_data(disc.get_var_full_name('cache_file_path', disc._data_in), self.VALUE,
-                                         cache_file_path, check_value=False)
+                        self.dm.set_data(disc.get_var_full_name(
+                            'cache_type', disc._data_in), self.VALUE, cache_type, check_value=False)
+                        self.dm.set_data(disc.get_var_full_name(
+                            'cache_file_path', disc._data_in), self.VALUE, cache_file_path, check_value=False)
+
 
             # Debug mode
             debug_mode = self.get_sosdisc_inputs('debug_mode')
@@ -584,7 +588,8 @@ class SoSDiscipline(MDODiscipline):
         else:
             disc.cache = None
             if cache_type != 'None':
-                disc.set_cache_policy(cache_type=cache_type, cache_hdf_file=cache_hdf_file)
+                disc.set_cache_policy(
+                    cache_type=cache_type, cache_hdf_file=cache_hdf_file)
 
     def setup_sos_disciplines(self):
         '''
@@ -631,6 +636,22 @@ class SoSDiscipline(MDODiscipline):
 
     def get_data_out(self):
         return self._data_out
+
+    def get_data_io_with_full_name(self, io_type):
+        data_io_short_name = self.get_data_io_dict(io_type)
+        data_io_full_name = {self.get_var_full_name(
+            var_name, data_io_short_name): value_dict for var_name, value_dict in data_io_short_name.items()}
+
+        return data_io_full_name
+
+    def get_data_with_full_name(self, io_type, full_name, data_name=None):
+
+        data_io_full_name = self.get_data_io_with_full_name(io_type)
+
+        if data_name is None:
+            return data_io_full_name[full_name]
+        else:
+            return data_io_full_name[full_name][data_name]
 
     def _update_with_values(self, to_update, update_with, update_dm=False):
         ''' update <to_update> 'value' field with <update_with>
@@ -1072,7 +1093,11 @@ class SoSDiscipline(MDODiscipline):
 
             elif index_y_column is None and index_x_column is not None:
                 self.jac[new_y_key][new_x_key][:, index_x_column *
+<<<<<<< Upstream, based on origin/develop
                                                   lines_nb_x:(index_x_column + 1) * lines_nb_x] = value
+=======
+                                               lines_nb_x:(index_x_column + 1) * lines_nb_x] = value
+>>>>>>> e9a70ac Add check var data in desc_in for coupling variables and shared input variables
 
                 self.jac_boundaries.update({f'{new_y_key},{y_column}': {'start': 0,
                                                                         'end': -1},
