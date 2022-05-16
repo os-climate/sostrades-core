fe420f21cfaa527f460c7d3d5061cde968aadb14
MARGERIT Daniel <NG947AC@eu.airbus.corp>
2022-05-16 15:52:51 +0200

    run_usecase put to False in Multi scen of SA because of L1 twice run error

--- sos_trades_core/sos_processes/test/test_multiscenario_of_doe_eval/usecase.py
+++ sos_trades_core/sos_processes/test/test_multiscenario_of_doe_eval/usecase.py
@@ -17,6 +17,7 @@ limitations under the License.
 from sos_trades_core.study_manager.study_manager import StudyManager
 import pandas as pd
 
+
 class Study(StudyManager):
     '''This is an example of usecase study for
      the test_multiscenario_of_DoE_Eval specify process.
@@ -24,9 +25,9 @@ class Study(StudyManager):
     It uses the 2 wrapped disciplines : disc1_scenario.Disc1
      (orchestrated by the test_disc1_scenario process) and disc3_scenario.Disc3.
     '''
-    def __init__(self, execution_engine=None):
-        super().__init__(__file__, execution_engine=execution_engine)
 
+    def __init__(self, execution_engine=None):
+        super().__init__(__file__, execution_engine=execution_engine, run_usecase=False)
 
     def setup_usecase(self):
         ######### Numerical values   ####
@@ -41,39 +42,42 @@ class Study(StudyManager):
 
         constant = 3
         power = 2
-        z_1 =  1.2
-        z_2 =  1.5
+        z_1 = 1.2
+        z_2 = 1.5
 
         my_doe_algo = "lhs"
         n_samples = 4
 
-
         ######### Selection of variables and DS  ####
         input_selection_z_scenario_1 = {
             'selected_input': [False, False, False, True, False, False],
-            'full_name': ['x', 'a','b','multi_scenarios.scenario_1.Disc3.z','constant','power']}
-        input_selection_z_scenario_1 = pd.DataFrame(input_selection_z_scenario_1)
+            'full_name': ['x', 'a', 'b', 'multi_scenarios.scenario_1.Disc3.z', 'constant', 'power']}
+        input_selection_z_scenario_1 = pd.DataFrame(
+            input_selection_z_scenario_1)
 
         input_selection_z_scenario_2 = {
             'selected_input': [False, False, False, True, False, False],
-            'full_name': ['x', 'a','b','multi_scenarios.scenario_2.Disc3.z','constant','power']}
-        input_selection_z_scenario_2 = pd.DataFrame(input_selection_z_scenario_2)
+            'full_name': ['x', 'a', 'b', 'multi_scenarios.scenario_2.Disc3.z', 'constant', 'power']}
+        input_selection_z_scenario_2 = pd.DataFrame(
+            input_selection_z_scenario_2)
 
         output_selection_o_scenario_1 = {
             'selected_output': [False, False, True],
             'full_name': ['indicator', 'y', 'multi_scenarios.scenario_1.o']}
-        output_selection_o_scenario_1 = pd.DataFrame(output_selection_o_scenario_1)
+        output_selection_o_scenario_1 = pd.DataFrame(
+            output_selection_o_scenario_1)
 
         output_selection_o_scenario_2 = {
             'selected_output': [False, False, True],
             'full_name': ['indicator', 'y', 'multi_scenarios.scenario_2.o']}
-        output_selection_o_scenario_2 = pd.DataFrame(output_selection_o_scenario_2)
+        output_selection_o_scenario_2 = pd.DataFrame(
+            output_selection_o_scenario_2)
 
         dspace_dict_z = {'variable': ['z'],
-                          'lower_bnd': [0.],
-                          'upper_bnd': [10.],
-                          'enable_variable': [True],
-                          'activated_elem': [[True]]}
+                         'lower_bnd': [0.],
+                         'upper_bnd': [10.],
+                         'enable_variable': [True],
+                         'activated_elem': [[True]]}
         dspace_z = pd.DataFrame(dspace_dict_z)
 
         my_name_list = ['name_1', 'name_2']
@@ -94,36 +98,38 @@ class Study(StudyManager):
         dict_values[f'{prefix}.name_1.x_trade'] = my_x_trade
         dict_values[f'{prefix}.trade_variables'] = my_trade_variables
 
-        dict_values[f'{prefix}.name_list'] =  my_name_list
+        dict_values[f'{prefix}.name_list'] = my_name_list
 
         dict_values[f'{prefix}.scenario_1.DoE_Eval.Disc1.name_1.b'] = b_1
         dict_values[f'{prefix}.scenario_1.DoE_Eval.Disc1.name_2.b'] = b_2
-
 
         dict_values[f'{prefix}.scenario_2.DoE_Eval.Disc1.name_1.b'] = b_1
         dict_values[f'{prefix}.scenario_2.DoE_Eval.Disc1.name_2.b'] = b_2
 
         dict_values[f'{prefix}.scenario_1.DoE_Eval.Disc3.constant'] = constant
         dict_values[f'{prefix}.scenario_1.DoE_Eval.Disc3.power'] = power
-        dict_values[f'{prefix}.scenario_1.Disc3.z'] = z_1 # reference value (computed in any case)
+        # reference value (computed in any case)
+        dict_values[f'{prefix}.scenario_1.Disc3.z'] = z_1
 
         dict_values[f'{prefix}.scenario_2.DoE_Eval.Disc3.constant'] = constant
         dict_values[f'{prefix}.scenario_2.DoE_Eval.Disc3.power'] = power
-        dict_values[f'{prefix}.scenario_2.Disc3.z'] = z_2 # reference value (computed in any case)
+        # reference value (computed in any case)
+        dict_values[f'{prefix}.scenario_2.Disc3.z'] = z_2
 
         dict_values[f'{prefix}.scenario_1.DoE_Eval.sampling_algo'] = my_doe_algo
         dict_values[f'{prefix}.scenario_1.DoE_Eval.eval_inputs'] = input_selection_z_scenario_1
         dict_values[f'{prefix}.scenario_1.DoE_Eval.eval_outputs'] = output_selection_o_scenario_1
-
 
         dict_values[f'{prefix}.scenario_2.DoE_Eval.sampling_algo'] = my_doe_algo
         dict_values[f'{prefix}.scenario_2.DoE_Eval.eval_inputs'] = input_selection_z_scenario_2
         dict_values[f'{prefix}.scenario_2.DoE_Eval.eval_outputs'] = output_selection_o_scenario_2
 
         dict_values[f'{prefix}.scenario_1.DoE_Eval.design_space'] = dspace_z
-        dict_values[f'{prefix}.scenario_1.DoE_Eval.algo_options'] = {'n_samples': n_samples}
+        dict_values[f'{prefix}.scenario_1.DoE_Eval.algo_options'] = {
+            'n_samples': n_samples}
         dict_values[f'{prefix}.scenario_2.DoE_Eval.design_space'] = dspace_z
-        dict_values[f'{prefix}.scenario_2.DoE_Eval.algo_options'] = {'n_samples': n_samples}
+        dict_values[f'{prefix}.scenario_2.DoE_Eval.algo_options'] = {
+            'n_samples': n_samples}
 
         return [dict_values]
 
