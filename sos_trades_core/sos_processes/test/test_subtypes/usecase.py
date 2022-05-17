b32195e2aa20f5f0771137e0932d54f67492ca01
geoffreydelric <geoffrey.delric.external@airbus.com>
2022-05-17 14:57:06 +0200

    creating new process with subtypes

---
+++ sos_trades_core/sos_processes/test/test_subtypes/usecase.py
@@ -0,0 +1,35 @@
+'''
+Copyright 2022 Airbus SAS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+import pandas as pd
+from numpy import array
+
+from sos_trades_core.study_manager.study_manager import StudyManager
+
+
+class Study(StudyManager):
+
+    def __init__(self, run_usecase=False, execution_engine=None):
+        super().__init__(__file__, run_usecase=run_usecase, execution_engine=execution_engine)
+
+    def setup_usecase(self):
+
+        return {}
+
+
+if '__main__' == __name__:
+    uc_cls = Study(run_usecase=True)
+    uc_cls.load_data()
+    uc_cls.run()
