fef255ad7a81e09329575448979a80e1c670a4dd
MARGERIT Daniel <NG947AC@eu.airbus.corp>
2022-06-07 09:31:55 +0200

    proc builder Improve deo_eval and tests

--- sos_trades_core/sos_processes/test/test_disc0_doe_eval/process.py
+++ sos_trades_core/sos_processes/test/test_disc0_doe_eval/process.py
@@ -13,24 +13,34 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-# mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8
-# -- Generate test 1 process
+"""
+mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
+Generate a doe scenario
+"""
 from sos_trades_core.sos_processes.base_process_builder import BaseProcessBuilder
 
 
 class ProcessBuilder(BaseProcessBuilder):
     # ontology information
     _ontology_data = {
-        'label': 'sos_trades_core.sos_processes.test.test_proc_build_disc0',
+        'label': 'sos_trades_core.sos_processes.test.test_sellar_doe_eval',
         'description': '',
         'category': '',
         'version': '',
     }
 
     def get_builders(self):
+        '''
+        default initialisation test
+        '''
+        # add disciplines Sellar
         mod_path = 'sos_trades_core.sos_wrapping.test_discs.disc0.Disc0'
+        mods_dict = {'Disc0': mod_path}
+        builder_list = self.create_builder_list(mods_dict,
+                                                ns_dict={'ns_disc1': self.ee.study_name,
+                                                         'ns_doe_eval': f'{self.ee.study_name}.DoE_Eval'}
+                                                )
+        doe_eval_builder = self.ee.factory.create_evaluator_builder(
+            'DoE_Eval', 'doe_eval', builder_list)
 
-        disc_builder = self.ee.factory.get_builder_from_module(
-            'Disc0', mod_path)
-        self.ee.ns_manager.add_ns('ns_disc1', f'{self.ee.study_name}')
-        return [disc_builder]
+        return doe_eval_builder
