f681971426218321179a93e1b1075b2a2be77ed7
LEONARD Elise <NG878B4@eu.airbus.corp>
2022-07-12 10:54:46 +0200

    update gemseo import

--- sostrades_core/execution_engine/discipline_proxy.py
+++ sostrades_core/execution_engine/discipline_proxy.py
@@ -13,9 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-# from scipy.sparse.lil import lil_matrix
-# 
-# from gemseo.utils.derivatives.derivatives_approx import DisciplineJacApprox
+from scipy.sparse.lil import lil_matrix
+ 
+from gemseo.utils.derivatives.derivatives_approx import DisciplineJacApprox
 
 '''
 mode: python; py-indent-offset: 4; tab-width: 8; coding: utf-8
