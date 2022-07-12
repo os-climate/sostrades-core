f66d745709e2741da1a9e17836aecefd9c64cfe2
TCHUISSEU FONGANG Carlin <SP005188@eu.airbus.corp>
2022-07-12 16:12:57 +0200

    reactivating test 15 17 19

--- sostrades_core/tools/post_processing/charts/two_axes_chart_template.py
+++ sostrades_core/tools/post_processing/charts/two_axes_chart_template.py
@@ -20,8 +20,8 @@ Class that define a 2 dimensional chart template
 """
 
 # pylint: disable=line-too-long
-from sos_trades_core.tools.post_processing.post_processing_plotly_tooling import AbstractPostProcessingPlotlyTooling
-from sos_trades_core.tools.post_processing.post_processing_tools import convert_nan
+from sostrades_core.tools.post_processing.post_processing_plotly_tooling import AbstractPostProcessingPlotlyTooling
+from sostrades_core.tools.post_processing.post_processing_tools import convert_nan
 
 
 class SeriesTemplateException(Exception):
