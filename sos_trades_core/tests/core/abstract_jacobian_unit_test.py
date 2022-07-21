9bc93d5a8d3624296e1c22d2121f4f77baecb08f
Julien Souchard <NG92D02@TOLX302085985.tls.fr.eu.airbus.corp>
2022-07-21 17:28:55 +0200

    Update abstract_jacobian_unit_test.py

--- sos_trades_core/tests/core/abstract_jacobian_unit_test.py
+++ sos_trades_core/tests/core/abstract_jacobian_unit_test.py
@@ -153,8 +153,8 @@ class AbstractJacobianUnittest(unittest.TestCase, ABC):
                     entry.join()
             local_logger.info(
                 f'----------------- Git commit and push ------------------------')
-            os.system('git config user.name "Jacobian pickle dumper"')
-            #os.system('git config --global user.email "julien.souchard.external@airbus.com"')
+            os.system('git config --global user.email "julien.souchard.external@airbus.com"')
+            os.system('git config --global user.name "Jacobian pickle dumper"')
             for directory in directories:
                 os.system(f'git add ./{directory}/*.pkl')
             os.system(f'git commit -m "regeneration of jacobian pickles for {file_regex}"')
