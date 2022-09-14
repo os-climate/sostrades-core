09e21b9915ee4da538d12e31e49751ec0b40e69a
MARGERIT Daniel <NG947AC@eu.airbus.corp>
2022-09-14 19:04:59 +0200

    [proc build] update ontologie of processes

--- sos_trades_core/sos_processes/test/test_disc1_disc3_vs_MS_from_proc/usecase1.py
+++ sos_trades_core/sos_processes/test/test_disc1_disc3_vs_MS_from_proc/usecase1.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2022 Airbus SA
+Copyright 2022 Airbus SAS
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
