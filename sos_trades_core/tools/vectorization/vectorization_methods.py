5255b65aabfbce14233d7320a675cbf8d2d06dac
Etienne Tomiak <etienne.e.tomiak.external@airbus.com>
2022-09-16 12:26:12 +0200

    update vectorization method for aircraft breakdown

--- sos_trades_core/tools/vectorization/vectorization_methods.py
+++ sos_trades_core/tools/vectorization/vectorization_methods.py
@@ -23,7 +23,8 @@ from logging import Logger
 import pandas as pd
 from sos_trades_core.execution_engine.sos_discipline import SoSDiscipline
 
-GRANULARITY_COLUMN = 'PATH'
+BREAKDOWN_COLUMN = 'PATH'
+
 
 def get_parent_path(PATH):
     path_list = PATH.split('.')
@@ -34,20 +35,20 @@ def get_parent_path(PATH):
         return path_list
 
 
-def merge_df_dict_with_path(df_dict:dict)->pd.DataFrame:
+def merge_df_dict_with_path(df_dict: dict) -> pd.DataFrame:
     """Method to merge a dictionary of dataframe into a single dataframe.
-    A new column for the granularity is created and the dictionary key is used as the value for the resulting dataframe
+    A new column for the aircraft_breakdown is created and the dictionary key is used as the value for the resulting dataframe
 
     Args:
         df_dict (dict): dictionary of dataframe. All dataframe must have identical columns
 
     Returns:
-        pd.DataFrame: merged dataframe with a new column GRANULARITY_COLUMN with the dict key as value
-    """    
+        pd.DataFrame: merged dataframe with a new column BREAKDOWN_COLUMN with the dict key as value
+    """
     df_with_path = pd.DataFrame({})
     for key, df in df_dict.items():
         df_copy = df.copy(deep=True)
-        df_copy.insert(0,GRANULARITY_COLUMN,key)
+        df_copy.insert(0, BREAKDOWN_COLUMN, key)
         df_with_path = df_with_path.append(df_copy, ignore_index=True)
 
     return df_with_path
@@ -56,13 +57,13 @@ def merge_df_dict_with_path(df_dict:dict)->pd.DataFrame:
 def compute_parent_path_sum(df, path, based_on, columns_not_to_sum):
     all_columns = df.columns.to_list()
     col_to_sum = [val for val in all_columns if val not in columns_not_to_sum]
-    filtered_df = df.loc[df[GRANULARITY_COLUMN].str.startswith(path)]
+    filtered_df = df.loc[df[BREAKDOWN_COLUMN].str.startswith(path)]
     df_with_col_sum = filtered_df.groupby(based_on)[col_to_sum].sum().reset_index()
     df_with_col_not_sum = filtered_df[columns_not_to_sum]
     df_merged = pd.merge(
         df_with_col_sum, df_with_col_not_sum, on=based_on, how='left'
     ).drop_duplicates()
-    df_merged[GRANULARITY_COLUMN] = path
+    df_merged[BREAKDOWN_COLUMN] = path
     df_merged = df_merged[all_columns]
     df = df.append(df_merged)
     return df
@@ -70,7 +71,7 @@ def compute_parent_path_sum(df, path, based_on, columns_not_to_sum):
 
 # check compute sum of val for each possible parent paths except for columns_not_to_sum (with sum based on)
 def check_compute_parent_path_sum(df, columns_not_to_sum, based_on):
-    path_list = df[GRANULARITY_COLUMN].unique().tolist()
+    path_list = df[BREAKDOWN_COLUMN].unique().tolist()
     for path in path_list:
         path = get_parent_path(path)
         if path in path_list or len(path) == 0:
@@ -84,13 +85,18 @@ def check_compute_parent_path_sum(df, columns_not_to_sum, based_on):
 
 # return input_parameter filtered on PATH parameter
 # if parent_path_admissible, return iput_parameter filtered on parent path if existing
-def get_inputs_for_path(input_parameter:pd.DataFrame, PATH:str, parent_path_admissible:bool=False, unique_value:bool = False):
+def get_inputs_for_path(
+    input_parameter: pd.DataFrame,
+    PATH: str,
+    parent_path_admissible: bool = False,
+    unique_value: bool = False,
+):
     filtered_input_parameter = None
-    if GRANULARITY_COLUMN in input_parameter:
+    if BREAKDOWN_COLUMN in input_parameter:
         while filtered_input_parameter is None and len(PATH) > 0:
-            if PATH in input_parameter[GRANULARITY_COLUMN].unique():
+            if PATH in input_parameter[BREAKDOWN_COLUMN].unique():
                 filtered_input_parameter = input_parameter.loc[
-                    input_parameter[GRANULARITY_COLUMN] == PATH
+                    input_parameter[BREAKDOWN_COLUMN] == PATH
                 ]
             else:
                 if parent_path_admissible:
@@ -100,15 +106,19 @@ def get_inputs_for_path(input_parameter:pd.DataFrame, PATH:str, parent_path_admi
                         f'the path {PATH} is not found in PATH input_parameter column'
                     )
     else:
-        raise Exception(f"The column {GRANULARITY_COLUMN} is not found as an input_parameter column")
+        raise Exception(
+            f"The column {BREAKDOWN_COLUMN} is not found as an input_parameter column"
+        )
     if filtered_input_parameter is None:
         raise Exception("Can not find parent path")
     else:
-        filtered_input_parameter = filtered_input_parameter.drop(columns=['PATH']).reset_index(drop=True)
+        filtered_input_parameter = filtered_input_parameter.drop(
+            columns=['PATH']
+        ).reset_index(drop=True)
         # check if result dataframe is only one column
         if filtered_input_parameter.shape[1] == 1:
             # in that case, return only the column values as a list
-            values_list = filtered_input_parameter.iloc[:,0].values.tolist()
+            values_list = filtered_input_parameter.iloc[:, 0].values.tolist()
             if unique_value:
                 return values_list[0]
             else:
@@ -117,17 +127,43 @@ def get_inputs_for_path(input_parameter:pd.DataFrame, PATH:str, parent_path_admi
             return filtered_input_parameter
 
 
-
-def check_granularity_in_inputs(inputs_dict:dict, parameters_dict:dict, granularity_list:list,logger:Logger,sos_discipline:SoSDiscipline):
+def check_aircraft_breakdown_in_inputs(
+    inputs_dict: dict,
+    parameters_dict: dict,
+    aircraft_breakdown_tree: list,
+    logger: Logger,
+    sos_discipline: SoSDiscipline,
+):
+    aircraft_breakdown_list = flatten(nested_dict=aircraft_breakdown_tree)
     for param_name, conf_dict in parameters_dict.items():
-        parameter = inputs_dict.get(param_name,None)
+        parameter = inputs_dict.get(param_name, None)
         if parameter is not None:
-            if GRANULARITY_COLUMN in parameter:
-                for granularity in granularity_list:
-                    if not granularity in parameter[GRANULARITY_COLUMN].values:
-                        logger.warning(f'Granularity {granularity} is missing for parameter {param_name} for discipline {sos_discipline.get_disc_full_name()}')
+            if BREAKDOWN_COLUMN in parameter:
+                for aircraft_breakdown in aircraft_breakdown_list:
+                    if not aircraft_breakdown in parameter[BREAKDOWN_COLUMN].values:
+                        logger.warning(
+                            f'Aircraft Breakdown {aircraft_breakdown} is missing for parameter {param_name} for discipline {sos_discipline.get_disc_full_name()}'
+                        )
             else:
-                logger.warning(f'Parameter {param_name} does not have the column {GRANULARITY_COLUMN}, impossible to check if all values are present for discipline {sos_discipline.get_disc_full_name()}')
+                logger.warning(
+                    f'Parameter {param_name} does not have the column {BREAKDOWN_COLUMN}, impossible to check if all values are present for discipline {sos_discipline.get_disc_full_name()}'
+                )
 
         else:
-            logger.warning(f'Parameter {param_name} is not in input dictionary for discipline {sos_discipline.get_disc_full_name()}')
+            logger.warning(
+                f'Parameter {param_name} is not in input dictionary for discipline {sos_discipline.get_disc_full_name()}'
+            )
+
+
+def flatten(nested_dict, flatten_list=None, parent_key=None):
+    if flatten_list is None:
+        flatten_list = []
+    for key, sub_dict in sorted(nested_dict.items()):
+        if parent_key is None:
+            full_key = key
+        else:
+            full_key = f'{parent_key}.{key}'
+        flatten_list.append(full_key)
+        if sub_dict:
+            flatten(sub_dict, flatten_list, parent_key=full_key)
+    return flatten_list
