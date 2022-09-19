63b2ac564657c053c6d2bf6e15d420390a7b2a99
MARGERIT Daniel <NG947AC@eu.airbus.corp>
2022-09-19 16:22:57 +0200

    [proc build] vs_MS version with usecase of disc1-disc3 but ns provided in process

--- sos_trades_core/sos_processes/test/test_driver_build_vs_MS_empty/usecase_d1d3_with_input_setup.py
+++ sos_trades_core/sos_processes/test/test_driver_build_vs_MS_empty/usecase_d1d3_with_input_setup.py
@@ -42,39 +42,49 @@ class Study(StudyManager):
         input_ns = 'ns_scatter_scenario'
         output_name = 'scenario_name'
         scatter_ns = 'ns_scenario'  # not used
-        ns_to_update = ['ns_disc3', 'ns_out_disc3', 'ns_ac']
-        scenario_map = {'input_name': scenario_map_name,
-                        'input_ns': input_ns,
-                        'output_name': output_name,
-                        'scatter_ns': scatter_ns,
-                        'gather_ns': input_ns,
-                        'ns_to_update': ns_to_update}
+        ns_to_update = ['ns_data_ac', 'ns_ac', 'ns_disc3', 'ns_out_disc3']
+        scenario_map = {
+            'input_name': scenario_map_name,
+            'input_ns': input_ns,
+            'output_name': output_name,
+            'scatter_ns': scatter_ns,
+            'gather_ns': input_ns,
+            'ns_to_update': ns_to_update,
+        }
 
         ######### Numerical values   ####
-        x = 2.
-        a = 3.
-        b = 4.
+        x1 = 2.0
+        x2 = 4.0
+        a1 = 3.0
+        b1 = 4.0
+        a2 = 6.0
+        b2 = 2.0
         scenario_list = ['scenario_1', 'scenario_2']
         ######### Fill the dictionary for dm   ####
         dict_values = {}
 
-        dict_values[f'{self.study_name}.vs_MS.sub_process_inputs'] = sub_process_inputs_dict
+        dict_values[
+            f'{self.study_name}.vs_MS.sub_process_inputs'
+        ] = sub_process_inputs_dict
         dict_values[f'{self.study_name}.vs_MS.scenario_map'] = scenario_map
 
         dict_values[f'{self.study_name}.vs_MS.scenario_list'] = scenario_list
 
-        dict_values[f'{self.study_name}.a'] = a
-        dict_values[f'{self.study_name}.x'] = x
+        dict_values[f'{self.study_name}.scenario_1.a'] = a1
+        dict_values[f'{self.study_name}.scenario_1.x'] = x1
 
-        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc1.b'] = b
-        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc3.constant'] = 3.
-        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc3.power'] = 2
+        dict_values[f'{self.study_name}.scenario_2.a'] = a2
+        dict_values[f'{self.study_name}.scenario_2.x'] = x2
+
+        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc1.b'] = b1
+        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc3.constant'] = 3
+        dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc3.power'] = 1
         dict_values[f'{self.study_name}.vs_MS.scenario_1.Disc3.z'] = 1.2
 
-        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc1.b'] = b
-        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc3.constant'] = 3.
+        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc1.b'] = b2
+        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc3.constant'] = 2
         dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc3.power'] = 2
-        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc3.z'] = 1.5
+        dict_values[f'{self.study_name}.vs_MS.scenario_2.Disc3.z'] = 1.2
 
         return [dict_values]
 
