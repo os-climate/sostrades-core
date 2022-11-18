0fb85b01216a757881593aeea237ff87408909f5
Valentin JONCQUIERES <valentin.joncquieres.external@airbus.com>
2022-11-18 14:24:21 +0100

    Refactor test_24 with scatter as a tool

--- sostrades_core/tests/l0_test_24_configure_scatter.py
+++ sostrades_core/tests/l0_test_24_configure_scatter.py
@@ -17,7 +17,7 @@ limitations under the License.
 mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
 '''
 import unittest
-
+import pandas as pd
 from sostrades_core.execution_engine.execution_engine import ExecutionEngine
 
 
@@ -30,7 +30,7 @@ class TestScatterDiscipline(unittest.TestCase):
         '''
         Initialize third data needed for testing
         '''
-        self.name = 'Scatter'
+        self.name = 'Root'
         self.ee = ExecutionEngine(self.name)
 
         # set scatter build map
@@ -51,9 +51,10 @@ class TestScatterDiscipline(unittest.TestCase):
         cls_list = self.ee.factory.get_builder_from_process(repo='sostrades_core.sos_processes.test',
                                                             mod_id=sub_proc)
 
+        self.ms_name = 'multiscenarios'
         # create scatter builder with map and coupling process
-        scatter_list = self.ee.factory.create_multi_scatter_builder_from_list(
-            'AC_list', cls_list, autogather=True)
+        scatter_list = self.ee.factory.create_scatter_driver_with_tool(
+            self.ms_name, cls_list,  'AC_list')
 
         # set scatter builder to root process
         self.ee.factory.set_builders_to_coupling_builder(scatter_list)
@@ -75,16 +76,15 @@ class TestScatterDiscipline(unittest.TestCase):
         disciplines_list = []
 
         disciplines_list.append(self.name)
-        disciplines_list.append(
-            f'{self.name}.Disc1')
-        disciplines_list.append(
-            f'{self.name}.Disc2')
+        disciplines_list.append(f'{self.name}.{self.ms_name}')
 
         for aircraft in list_aircraft:
             disciplines_list.append(
-                f'{self.name}.Disc1.{aircraft}')
+                f'{self.name}.{self.ms_name}.{aircraft}')
             disciplines_list.append(
-                f'{self.name}.Disc2.{aircraft}')
+                f'{self.name}.{self.ms_name}.{aircraft}.Disc1')
+            disciplines_list.append(
+                f'{self.name}.{self.ms_name}.{aircraft}.Disc2')
 
         disciplines_list.sort()
 
@@ -93,9 +93,11 @@ class TestScatterDiscipline(unittest.TestCase):
     def test_01_raw_initialisation(self):
 
         private_values_multiproduct = {
-            f'{self.name}.AC_list': ['A1']}
+            f'{self.name}.{self.ms_name}.builder_mode': 'multi_instance',
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True],
+                                                                     'scenario_name': ['A1']})}
 
-        self.ee.dm.set_values_from_dict(private_values_multiproduct)
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         self.ee.configure()
 
@@ -113,9 +115,11 @@ class TestScatterDiscipline(unittest.TestCase):
     def test_02_multiinstance_modification_remove_one_aircraft_1(self):
 
         private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_1}
+            f'{self.name}.{self.ms_name}.builder_mode': 'multi_instance',
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True] * len(self.list_aircraft_1),
+                                                                     'scenario_name': self.list_aircraft_1})}
 
-        self.ee.dm.set_values_from_dict(private_values_multiproduct)
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         self.ee.configure()
 
@@ -133,9 +137,10 @@ class TestScatterDiscipline(unittest.TestCase):
 
         #-- remove on aircraft
         private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_remove_1_1}
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True] * len(self.list_aircraft_remove_1_1),
+                                                                     'scenario_name': self.list_aircraft_remove_1_1})}
 
-        self.ee.dm.set_values_from_dict(private_values_multiproduct)
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         self.ee.configure()
 
@@ -154,9 +159,11 @@ class TestScatterDiscipline(unittest.TestCase):
     def test_03_multiinstance_modification_remove_one_aircraft_2(self):
 
         private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_1}
+            f'{self.name}.{self.ms_name}.builder_mode': 'multi_instance',
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True] * len(self.list_aircraft_1),
+                                                                     'scenario_name': self.list_aircraft_1})}
 
-        self.ee.dm.set_values_from_dict(private_values_multiproduct)
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         self.ee.configure()
 
@@ -174,9 +181,10 @@ class TestScatterDiscipline(unittest.TestCase):
 
         #-- remove on aircraft
         private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_remove_1_2}
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True] * len(self.list_aircraft_remove_1_2),
+                                                                     'scenario_name': self.list_aircraft_remove_1_2})}
 
-        self.ee.dm.set_values_from_dict(private_values_multiproduct)
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         self.ee.configure()
 
@@ -194,11 +202,18 @@ class TestScatterDiscipline(unittest.TestCase):
 
     def test_04_multiinstance_modification_remove_all_aircraft(self):
 
+        private_values_multiproduct = {
+            f'{self.name}.{self.ms_name}.builder_mode': 'multi_instance'}
+
+        self.ee.load_study_from_input_dict(private_values_multiproduct)
+
         raw_dm_values = list(self.ee.dm.data_id_map.keys())
         raw_dm_values.sort()
 
         private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_1}
+            f'{self.name}.{self.ms_name}.builder_mode': 'multi_instance',
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [True] * len(self.list_aircraft_1),
+                                                                     'scenario_name': self.list_aircraft_1})}
 
         self.ee.load_study_from_input_dict(private_values_multiproduct)
 
@@ -215,14 +230,14 @@ class TestScatterDiscipline(unittest.TestCase):
 
         #-- remove all aircraft
         private_values_multiproduct = {
-            f'{self.name}.AC_list': []}
+            f'{self.name}.{self.ms_name}.scenario_df': pd.DataFrame({'selected_scenario': [False] * len(self.list_aircraft_1),
+                                                                     'scenario_name': self.list_aircraft_1})}
 
         self.ee.load_study_from_input_dict(private_values_multiproduct)
 
         exp_tv_list = [f'Nodes representation for Treeview {self.name}',
                        f'|_ {self.name}',
-                       f'\t|_ Disc1',
-                       f'\t|_ Disc2']
+                       f'\t|_ {self.ms_name}']
         exp_tv_str = '\n'.join(exp_tv_list)
         assert exp_tv_str == self.ee.display_treeview_nodes()
 
@@ -239,81 +254,12 @@ class TestScatterDiscipline(unittest.TestCase):
         last_dm_values = list(self.ee.dm.data_id_map.keys())
         last_dm_values.sort()
 
-        # Except dict out of gather
-        last_dm_values.remove('Scatter.y_dict')
-        last_dm_values.remove('Scatter.z_dict')
         self.assertListEqual(raw_dm_values, last_dm_values,
                              'After removing discipline, data manager variables list is different than raw list')
-
-    def test_05_multiinstance_modification_remove_one_aircraft_1_on_coupling_per_scenario(self):
-        disc_list_full = ['Scatter', 'Scatter.scatter_node',
-                          'Scatter.scatter_node.CH19_H2', 'Scatter.scatter_node.CH19_H2.Disc1', 'Scatter.scatter_node.CH19_H2.Disc2',
-                          'Scatter.scatter_node.CH19_Kero', 'Scatter.scatter_node.CH19_Kero.Disc1', 'Scatter.scatter_node.CH19_Kero.Disc2']
-        disc_list_1_1 = disc_list_full[:-3]
-
-        self.exec_eng = ExecutionEngine(self.name)
-
-        # set scatter build map
-        mydict = {'input_name': 'AC_list',
-                  'input_type': 'string_list',
-                  'input_ns': 'ns_barrierr',
-                  'output_name': 'ac_name',
-                  'scatter_ns': 'ns_ac',
-                  'gather_ns': 'ns_barrierr'
-                  }
-        self.exec_eng.smaps_manager.add_build_map('AC_list', mydict)
-
-        # set namespace definition
-        self.exec_eng.ns_manager.add_ns('ns_barrierr', self.name)
-        # get coupling process builder
-        sub_proc = 'test_disc1_disc2_coupling'
-        cls_list = self.exec_eng.factory.get_builder_from_process(repo='sostrades_core.sos_processes.test',
-                                                                  mod_id=sub_proc)
-
-        # create scatter builder with map and coupling process
-        scatter_node = self.exec_eng.factory.create_scatter_builder(
-            'scatter_node', 'AC_list', cls_list, coupling_per_scenario=True)
-
-        # set scatter builder to root process
-        self.exec_eng.factory.set_builders_to_coupling_builder(scatter_node)
-        self.exec_eng.configure()
-
-        private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_1}
-
-        self.exec_eng.dm.set_values_from_dict(private_values_multiproduct)
-
-        self.exec_eng.configure()
-
-        print('Treeview for test test_03_multiinstance_modification_remove_one_aircraft_1')
-        self.exec_eng.display_treeview_nodes()
-
-        disciplines_list = list(self.exec_eng.dm.disciplines_id_map.keys())
-        disciplines_list.sort()
-
-        self.assertListEqual(disc_list_full, disciplines_list,
-                             'Discipline between reference and generated are different')
-
-        #-- remove on aircraft
-        private_values_multiproduct = {
-            f'{self.name}.AC_list': self.list_aircraft_remove_1_1}
-
-        self.exec_eng.dm.set_values_from_dict(private_values_multiproduct)
-
-        self.exec_eng.configure()
-
-        print('Treeview for test test_03_multiinstance_modification_remove_one_aircraft_1')
-        self.exec_eng.display_treeview_nodes()
-
-        disciplines_list = list(self.exec_eng.dm.disciplines_id_map.keys())
-        disciplines_list.sort()
-
-        self.assertListEqual(disc_list_1_1, disciplines_list,
-                             'Discipline between reference and generated are different')
 
 
 if '__main__' == __name__:
     cls = TestScatterDiscipline()
     cls.setUp()
-    cls.test_05_multiinstance_modification_remove_one_aircraft_1_on_coupling_per_scatter()
+    cls.test_04_multiinstance_modification_remove_all_aircraft()
     cls.tearDown()
