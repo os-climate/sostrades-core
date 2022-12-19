610cfc3d8e17febe4dab4f839e9fa7e0baf133b0
MARGERIT Daniel <NG947AC@eu.airbus.corp>
2022-12-19 16:25:58 +0100

    Add three processes and associated usecases based on Test 79 on namespace handling

---
+++ sostrades_core/sos_processes/test/test_same_discipline/process.py
@@ -0,0 +1,50 @@
+'''
+Copyright 2022 Airbus SAS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+"""
+mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
+Generate an optimization scenario
+"""
+from sostrades_core.sos_processes.base_process_builder import BaseProcessBuilder
+
+
+class ProcessBuilder(BaseProcessBuilder):
+
+    # ontology information
+    _ontology_data = {
+        'label': 'Core Test Process: Same Discipline ',
+        'description': '',
+        'category': '',
+        'version': '',
+    }
+
+    def get_builders(self):
+        '''
+        default initialisation test
+        '''
+        ns_7 = self.ee.ns_manager.add_ns(
+            'ns_protected', f'{self.ee.study_name}.Disc7')
+        mod_list = 'sostrades_core.sos_wrapping.test_discs.disc7_wo_df.Disc7'
+        disc7_builder = self.ee.factory.get_builder_from_module(
+            'Disc7', mod_list)
+        disc7_builder.associate_namespaces(ns_7)
+
+        ns_72 = self.ee.ns_manager.add_ns(
+            'ns_protected', f'{self.ee.study_name}.Disc72')
+        disc7_builder2 = self.ee.factory.get_builder_from_module(
+            'Disc72', mod_list)
+        disc7_builder2.associate_namespaces(ns_72)
+
+        return [disc7_builder, disc7_builder2]
---
+++ sostrades_core/sos_processes/test/test_same_sub_process_inside_a_process/process.py
@@ -0,0 +1,51 @@
+'''
+Copyright 2022 Airbus SAS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+"""
+mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
+Generate an optimization scenario
+"""
+from sostrades_core.sos_processes.base_process_builder import BaseProcessBuilder
+
+
+class ProcessBuilder(BaseProcessBuilder):
+
+    # ontology information
+    _ontology_data = {
+        'label': 'Core Test Process: Same Subprocess inside a process',
+        'description': '',
+        'category': '',
+        'version': '',
+    }
+
+    def get_builders(self):
+        '''
+        default initialisation test
+        '''
+        disc_dir = 'sostrades_core.sos_wrapping.test_discs.'
+        mods_dict = {'proc1.Disc2': disc_dir + 'disc2.Disc2',
+                     'proc1.Disc1': disc_dir + 'disc1.Disc1'}
+        proc_builder = BaseProcessBuilder(self.ee)
+        builder_list = proc_builder.create_builder_list(
+            mods_dict, ns_dict={'ns_ac': f'{self.ee.study_name}.proc1'}, associate_namespace=True)
+
+        mods_dict = {'proc2.Disc2': disc_dir + 'disc2.Disc2',
+                     'proc2.Disc1': disc_dir + 'disc1.Disc1'}
+        builder_list2 = proc_builder.create_builder_list(
+            mods_dict, ns_dict={'ns_ac': f'{self.ee.study_name}.proc2'}, associate_namespace=True)
+
+        builder_list.extend(builder_list2)
+
+        return builder_list
