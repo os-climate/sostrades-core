69f584ac87b0b8c4369eab442b63ddbac0571c27
DELRIC Geoffrey <GDELRVQ4@eu.airbus.corp>
2023-01-17 17:23:15 +0100

    add file unit_conversion

---
+++ sostrades_core/tools/conversion/unit_conversion.py
@@ -0,0 +1,15 @@
+from cmath import pi
+
+
+class UnitConversion:
+
+    # conversion unit constant
+    nm_to_km = 1.85  # nautical mile to kilometer
+    m_to_ft = 3.28  # meter to feet
+    lb_to_kg = 0.45  # pounds to kilograms
+    b_to_hpa = 1000  # bar to hectopascal
+    ft_per_min_to_m_per_s = 0.00508  # feet per minute to meter per second
+    m_per_s_to_km_per_h = 3.6  # meter per second to kilometer per hour
+    deg_to_rad = pi / 180
+    kts_to_ms = m_per_s_to_km_per_h / nm_to_km
+    gravity = 9.81
