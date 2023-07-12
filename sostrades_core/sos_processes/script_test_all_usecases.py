a4f3b008f8a782328026bf6929cc50d0dd0c1cfa
Yasser BEKKALI <yasser.bekkali@capgemini.com>
2023-07-12 14:09:35 +0200

    no need to dump study when testing post processing

--- sostrades_core/sos_processes/script_test_all_usecases.py
+++ sostrades_core/sos_processes/script_test_all_usecases.py
@@ -424,10 +424,9 @@ def test_post_processing_study(study: BaseStudyManager, force_run: bool) -> tupl
     if study.run_usecase or force_run:
         try:
             # study.load_data(from_path=dump_dir) # already done in multiple_configure i think
-            study.run(logger_level=logging.DEBUG, dump_study=True, for_test=False)
+            study.run(logger_level=logging.DEBUG, dump_study=False, for_test=False)
         except Exception as e:
             raise Exception(f'\nERROR during first run of usecase {study.study_full_path}: {e}')
-        study.run(logger_level=logging.DEBUG, )
     else:
         logging.info(f'{study.study_full_path} is configured not to run, skipping run for post processing test.')
 
