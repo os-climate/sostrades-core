44af1eeb6fc6450d426a2f87de62d5db1e7d065d
magueylard <marie-audrey.gueylard@capgemini.com>
2023-10-12 11:07:02 +0200

    clean usecases after display_option refacto

--- sostrades_core/sos_processes/test/tests_driver_eval/mono/test_mono_driver_sample_generator_sellar_disc1_disc2/process.py
+++ sostrades_core/sos_processes/test/tests_driver_eval/mono/test_mono_driver_sample_generator_sellar_disc1_disc2/process.py
@@ -23,7 +23,7 @@ from sostrades_core.sos_processes.base_process_builder import BaseProcessBuilder
 class ProcessBuilder(BaseProcessBuilder):
     # ontology information
     _ontology_data = {
-        'label': 'Core Test simple1 simple2 Sellar Generator Eval',
+        'label': 'Core Test simple1 simple2 Sellar Mono Instance Eval with Sample Generator',
         'description': '',
         'category': '',
         'version': '',
