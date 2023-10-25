d28f87dfb889996963449933f6dacbf8bf00f17a
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2023-10-25 17:46:01 +0200

    create simple sample generator and a step towards oo refacto of sample generator

--- sostrades_core/execution_engine/disciplines_wrappers/sample_generator_wrapper.py
+++ sostrades_core/execution_engine/disciplines_wrappers/sample_generator_wrapper.py
@@ -34,6 +34,7 @@ mode: python; py-indent-offset: 4; tab-width: 8; coding: utf-8
 '''
 
 from sostrades_core.execution_engine.sos_wrapp import SoSWrapp
+from sostrades_core.execution_engine.sample_generators.simple_sample_generator import SimpleSampleGenerator
 from sostrades_core.execution_engine.sample_generators.doe_sample_generator import DoeSampleGenerator
 from sostrades_core.execution_engine.sample_generators.cartesian_product_sample_generator import \
     CartesianProductSampleGenerator
@@ -124,11 +125,19 @@ class SampleGeneratorWrapper(SoSWrapp):
     PARALLEL_OPTIONS = 'parallel_options'
 
     SAMPLING_METHOD = 'sampling_method'
+    SIMPLE_SAMPLING_METHOD = 'simple'
     DOE_ALGO = 'doe_algo'
     CARTESIAN_PRODUCT = 'cartesian_product'
     GRID_SEARCH = 'grid_search'
     FULLFACT = 'fullfact'
-    available_sampling_methods = [DOE_ALGO, CARTESIAN_PRODUCT, GRID_SEARCH]
+    available_sampling_methods = [SIMPLE_SAMPLING_METHOD, DOE_ALGO, CARTESIAN_PRODUCT, GRID_SEARCH]
+
+    SAMPLE_GENERATOR_CLS = {
+        SIMPLE_SAMPLING_METHOD: SimpleSampleGenerator,
+        DOE_ALGO: DoeSampleGenerator,
+        CARTESIAN_PRODUCT: CartesianProductSampleGenerator,
+        GRID_SEARCH: CartesianProductSampleGenerator
+    }
 
     SAMPLING_GENERATION_MODE = 'sampling_generation_mode'
     AT_CONFIGURATION_TIME = 'at_configuration_time'
@@ -175,8 +184,7 @@ class SampleGeneratorWrapper(SoSWrapp):
     def __init__(self, sos_name, logger: logging.Logger):
         super().__init__(sos_name=sos_name, logger=logger)
         self.sampling_method = None
-        self.sample_generator_doe = None
-        self.sample_generator_cp = None
+        self.sample_generator = None
 
         self.sampling_generation_mode = None
 
@@ -208,7 +216,22 @@ class SampleGeneratorWrapper(SoSWrapp):
             # TODO: Here we start from scratch each time we switch from one method to the other
             #       ... but maybe we would like to keep selected eval_inputs or eval_inputs_cp ?
 
-            if self.sampling_method == self.DOE_ALGO:
+            if self.sampling_method == self.SIMPLE_SAMPLING_METHOD:
+                # Reset parameters of the other method to initial values
+                # (cleaning)
+                #TODO: move all of these to the corresponding tools !
+                self.previous_eval_inputs_cp = None
+                self.eval_inputs_cp_filtered = None
+                self.eval_inputs_cp_validity = True
+                self.selected_inputs = []
+                self.dict_desactivated_elem = {}
+
+                self.sampling_generation_mode = self.AT_CONFIGURATION_TIME
+                disc_in[self.SAMPLING_GENERATION_MODE][self.VALUE] = self.AT_CONFIGURATION_TIME
+                pass
+
+
+            elif self.sampling_method == self.DOE_ALGO:
                 # TODO: consider refactoring this in object-oriented fashion before implementing the more complex modes
                 # Reset parameters of the other method to initial values
                 # (cleaning)
@@ -303,16 +326,12 @@ class SampleGeneratorWrapper(SoSWrapp):
     
     def instantiate_sampling_tool(self):
         """
-           Instantiate SampleGenerator once and only if needed
+           Instantiate SampleGenerator only if needed
         """
-        # TODO: refactor OO?
-        if self.sampling_method == self.DOE_ALGO:
-            if self.sample_generator_doe is None:
-                self.sample_generator_doe = DoeSampleGenerator(logger=self.logger.getChild("DoeSampleGenerator"))
-        elif self.sampling_method in [self.CARTESIAN_PRODUCT, self.GRID_SEARCH]:
-            if self.sample_generator_cp is None:
-                self.sample_generator_cp = CartesianProductSampleGenerator(
-                    logger=self.logger.getChild("CartesianProductSampleGenerator"))
+        if self.sampling_method:
+            sample_generator_cls = self.SAMPLE_GENERATOR_CLS[self.sampling_method]
+            if self.sample_generator.__class__ != sample_generator_cls:
+                self.sample_generator = sample_generator_cls(logger=self.logger.getChild(sample_generator_cls.__name__))
 
     def get_algo_default_options(self, algo_name):
         """
@@ -322,7 +341,7 @@ class SampleGeneratorWrapper(SoSWrapp):
         # In get_options_and_default_values, it is already checked whether the algo_name belongs to the list of possible Gemseo
         # DoE algorithms
         if algo_name in get_available_doe_algorithms():
-            algo_options_desc_in, algo_options_descr_dict = self.sample_generator_doe.get_options_and_default_values(
+            algo_options_desc_in, algo_options_descr_dict = self.sample_generator.get_options_and_default_values(
                 algo_name)
             return algo_options_desc_in
         else:
@@ -455,7 +474,7 @@ class SampleGeneratorWrapper(SoSWrapp):
         """
         if self.sampling_method == self.DOE_ALGO:
             # Get possible values for sampling algorithm name
-            available_doe_algorithms = self.sample_generator_doe.get_available_algo_names()
+            available_doe_algorithms = self.sample_generator.get_available_algo_names()
             dynamic_inputs.update({'sampling_algo':
                                        {self.TYPE: 'string',
                                         self.STRUCTURING: True,
@@ -674,7 +693,7 @@ class SampleGeneratorWrapper(SoSWrapp):
         design_space = self.create_design_space(
             self.selected_inputs, dspace_df)
 
-        samples_gene_df = self.sample_generator_doe.generate_samples(
+        samples_gene_df = self.sample_generator.generate_samples(
             algo_name, algo_options, design_space)
         return samples_gene_df
 
@@ -860,7 +879,7 @@ class SampleGeneratorWrapper(SoSWrapp):
         """
         dict_of_list_values = self.eval_inputs_cp_filtered.set_index(
             'full_name').T.to_dict('records')[0]
-        samples_gene_df = self.sample_generator_cp.generate_samples(
+        samples_gene_df = self.sample_generator.generate_samples(
             dict_of_list_values)
         return samples_gene_df
 
