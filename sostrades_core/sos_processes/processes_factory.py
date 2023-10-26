8f06fb29049f828506e22a58c9cc64f3e9372bc6
ggoyon <guillaume.goyon@capgemini.com>
2023-10-26 16:39:44 +0200

    Added exc_info on critical error when module is missing

--- sostrades_core/sos_processes/processes_factory.py
+++ sostrades_core/sos_processes/processes_factory.py
@@ -16,7 +16,6 @@ limitations under the License.
 # mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8
 #-- process configuration class
 from typing import Optional
-import traceback
 import logging
 from importlib import import_module
 from pathlib import Path
@@ -236,12 +235,10 @@ class SoSProcessFactory:
                     f'Unable to load the following module {repository_module_name}')
 
         except ModuleNotFoundError as error:
-            traceback.print_stack()
             self.logger.critical(
-                f'Unable to load the following module {repository_module_name} : {str(error)}')
+                f'Unable to load the following module {repository_module_name} : {str(error)}', exc_info=error)
         except TypeError as error:
-            traceback.print_stack()
             self.logger.critical(
-                f'Unable to load the following module {repository_module_name} : {str(error)}')
+                f'Unable to load the following module {repository_module_name} : {str(error)}', exc_info=error)
 
         return repositories_by_process
