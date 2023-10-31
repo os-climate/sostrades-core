e199a2c2c8352a939f06293f349df27237547aaf
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2023-10-31 18:24:39 +0100

    delegating to sample generator the fill of eval inputs WITHOUT homogenization (so not really working as expected for cartesian_product even though tests pass because there was no possible inputs suggestion before for eval_inputs_cp  either)

--- sostrades_core/execution_engine/proxy_sample_generator.py
+++ sostrades_core/execution_engine/proxy_sample_generator.py
@@ -16,9 +16,12 @@ limitations under the License.
 '''
 mode: python; py-indent-offset: 4; tab-width: 8; coding: utf-8
 '''
-from copy import copy
+from copy import copy, deepcopy
 from sostrades_core.execution_engine.proxy_discipline import ProxyDiscipline
+from sostrades_core.execution_engine.disciplines_wrappers.sample_generator_wrapper import SampleGeneratorWrapper
+from sostrades_core.tools.gather.gather_tool import check_eval_io
 from sostrades_core.execution_engine.ns_manager import NS_SEP
+import pandas as pd
 
 
 class ProxySampleGeneratorException(Exception):
@@ -44,9 +47,44 @@ class ProxySampleGenerator(ProxyDiscipline):
         'version': '',
     }
 
-    # TODO: rewrite these functions for sample proxy migrating class variables etc.
-    def set_eval_in_possible_values(self, possible_values):
-        return self.mdo_discipline_wrapp.wrapper.set_eval_in_possible_values(possible_values)
+    MULTIPLIER_PARTICULE = "__MULTIPLIER__" # todo: to delete
+    EVAL_INPUTS = SampleGeneratorWrapper.EVAL_INPUTS
 
+    def set_eval_in_possible_values(self, possible_values):
+        # TODO: might want to refactor this eventually
+        if possible_values:
+            disc_in = self.get_data_in()
+            if self.EVAL_INPUTS in disc_in:
+                default_in_dataframe = pd.DataFrame({'selected_input': [False for _ in possible_values],
+                                                     'full_name': possible_values})
+                eval_input_new_dm = self.get_sosdisc_inputs(self.EVAL_INPUTS)
+                eval_inputs_f_name = self.get_var_full_name(self.EVAL_INPUTS, disc_in)
+
+                if eval_input_new_dm is None:
+                    self.dm.set_data(eval_inputs_f_name,
+                                     'value', default_in_dataframe, check_value=False)
+                # check if the eval_inputs need to be updated after a subprocess
+                # configure
+                elif set(eval_input_new_dm['full_name'].tolist()) != (set(default_in_dataframe['full_name'].tolist())):
+                    error_msg = check_eval_io(eval_input_new_dm['full_name'].tolist(), default_in_dataframe['full_name'].tolist(),
+                                       is_eval_input=True)
+                    if len(error_msg) > 0:
+                        for msg in error_msg:
+                            self.logger.warning(msg)
+                    default_dataframe = deepcopy(default_in_dataframe)
+                    already_set_names = eval_input_new_dm['full_name'].tolist()
+                    already_set_values = eval_input_new_dm['selected_input'].tolist()
+                    for index, name in enumerate(already_set_names):
+                        default_dataframe.loc[default_dataframe['full_name'] == name, 'selected_input'] = \
+                            already_set_values[
+                                index]  # this will filter variables that are not inputs of the subprocess
+                        if self.MULTIPLIER_PARTICULE in name:
+                            default_dataframe = default_dataframe.append(
+                                pd.DataFrame({'selected_input': [already_set_values[index]],
+                                              'full_name': [name]}), ignore_index=True)
+                    self.dm.set_data(eval_inputs_f_name,
+                                     'value', default_dataframe, check_value=False)
+
+    # TODO: rewrite these functions for sample proxy migrating class variables etc.
     def setup_sos_disciplines(self):
         super().setup_sos_disciplines()
