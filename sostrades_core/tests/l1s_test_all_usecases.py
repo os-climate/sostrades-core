e42475be61fdad33b93ce9c007c9bfa275494140
perrotcap <antoine.perrot@capgemini.com>
2023-11-20 15:27:40 +0100

    renamed test all usecases function to be able to run pytest l1s

--- sostrades_core/tests/l1s_test_all_usecases.py
+++ sostrades_core/tests/l1s_test_all_usecases.py
@@ -19,7 +19,7 @@ mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8
 '''
 import unittest
 import pprint
-from sostrades_core.sos_processes.script_test_all_usecases import test_all_usecases
+from sostrades_core.sos_processes.script_test_all_usecases import _test_all_usecases
 
 
 class TestUseCases(unittest.TestCase):
@@ -36,6 +36,6 @@ class TestUseCases(unittest.TestCase):
         self.maxDiff = None
 
     def test_all_usecases(self):
-        test_passed, output_error = test_all_usecases(processes_repo=self.processes_repo)
+        test_passed, output_error = _test_all_usecases(processes_repo=self.processes_repo)
         if not test_passed:
             raise Exception(f'{output_error}')
