03ab59060682f4c409b9ed89082b580c491ef316
perrotcap <antoine.perrot@capgemini.com>
2024-01-03 09:42:29 +0100

    fix header

--- sostrades_core/sos_wrapping/test_discs/sellar_for_design_var.py
+++ sostrades_core/sos_wrapping/test_discs/sellar_for_design_var.py
@@ -1,6 +1,6 @@
 '''
 Copyright 2022 Airbus SAS
-
+Modifications on 03/01/2024 Copyright 2023 Capgemini
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
