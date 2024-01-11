8dce1eef9ffb6a97ab691faef24022f107e7e035
benherry <benjamin.herry@capgemini.com>
2024-01-11 12:17:52 +0100

    [fix] header

--- sostrades_core/sos_processes/test/test_same_discipline/process.py
+++ sostrades_core/sos_processes/test/test_same_discipline/process.py
@@ -1,6 +1,6 @@
 '''
 Copyright 2022 Airbus SAS
-
+Modifications on 2024/01/11-2024/01/11 Copyright 2024 Capgemini
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
