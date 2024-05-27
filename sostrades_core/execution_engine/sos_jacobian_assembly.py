2fb2622db292bd2e4f5aafaae101cceb155dcef0
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2024-05-27 12:46:55 +0200

    inline implementation of m4 for jacobian allocation

--- sostrades_core/execution_engine/sos_jacobian_assembly.py
+++ sostrades_core/execution_engine/sos_jacobian_assembly.py
@@ -1,3 +1,4 @@
+from __future__ import annotations # TODO: for m4
 '''
 Copyright 2022 Airbus SAS
 Modifications on 2023/03/27-2024/02/20 Copyright 2023 Capgemini
@@ -18,6 +19,7 @@ limitations under the License.
 Coupled derivatives calculations
 ********************************
 """
+
 from memory_profiler import profile
 import gc
 
@@ -38,6 +40,43 @@ from gemseo.algos.linear_solvers.linear_problem import LinearProblem
 from sostrades_core.execution_engine.parallel_execution.sos_parallel_execution import SoSDiscParallelLinearization
 from sostrades_core.tools.conversion.conversion_sostrades_sosgemseo import convert_new_type_into_array
 
+# TODO: for m4
+USE_M4_CSR = True
+from typing import NamedTuple
+from collections.abc import Iterable
+from collections.abc import Iterator
+from scipy.sparse import csr_matrix, bmat, eye
+from numpy import fill_diagonal, ndarray
+
+from importlib.metadata import version
+from typing import Final
+from typing import Union
+
+from numpy import ndarray
+from packaging.version import Version
+from packaging.version import parse as parse_version
+from scipy.sparse import coo_matrix
+
+SCIPY_VERSION: Final[Version] = parse_version(version("scipy"))
+
+if parse_version("1.11") > SCIPY_VERSION:
+    from scipy.sparse import spmatrix
+
+    sparse_classes = (spmatrix,)
+    SparseArrayType = spmatrix
+
+else:
+    from scipy.sparse import sparray
+    from scipy.sparse import spmatrix
+
+    sparse_classes = (spmatrix, sparray)
+    SparseArrayType = Union[coo_matrix, spmatrix, sparray]
+
+array_classes = (ndarray, *sparse_classes)
+
+
+
+# END TODO: for m4
 
 def none_factory():
     """Returns None...
@@ -49,10 +88,28 @@ def default_dict_factory():
     """Instantiates a defaultdict(None) object."""
     return defaultdict(none_factory)
 
-
 class SoSJacobianAssembly(JacobianAssembly):
     """Assembly of Jacobians Typically, assemble disciplines's Jacobians into a system
     Jacobian."""
+
+    class JacobianPosition(NamedTuple):  # TODO: for m4 (whole subclass)
+        """The position of the discipline's Jacobians within the assembled Jacobian."""
+
+        row_slice: slice
+        """The row slice indicating where to position the disciplinary Jacobian within
+        the assembled Jacobian when defined as an array."""
+
+        column_slice: slice
+        """The column slice indicating where to position the disciplinary Jacobian
+        within the assembled Jacobian when defined as an array."""
+
+        row_index: int
+        """The row index of the disciplinary Jacobian within the assembled Jacobian when
+        defined blockwise."""
+
+        column_index: int
+        """The column index of the disciplinary Jacobian within the assembled Jacobian
+        when defined blockwise."""
 
     def __init__(self, coupling_structure, n_processes=1):
         self.n_processes = n_processes
@@ -61,14 +118,112 @@ class SoSJacobianAssembly(JacobianAssembly):
 
         self.parallel_linearize = SoSDiscParallelLinearization(
             self.coupling_structure.disciplines, n_processes=self.n_processes, use_threading=True)
-        
+
     def release_memory(self):
         gc.collect()
         self.coupled_system.release_memory()
 
     @profile
     def _dres_dvar_sparse_4_csr_new(self, residuals, variables, n_residuals, n_variables):
-        raise NotImplementedError()
+        # Fill in with zero blocks of appropriate dimension if necessary
+        variable_sizes = [self.sizes[variable_name] for variable_name in variables]
+        function_sizes = [self.sizes[function_name] for function_name in residuals]
+
+        # Initialize the block Jacobian with the appropriate structure
+        total_jacobian: list[list[Union[None, csr_matrix]]] = [
+            [None for _ in variables] for _ in residuals
+        ]
+        variable_sizes_0 = variable_sizes[0]
+        for i, function_size in enumerate(function_sizes):
+            total_jacobian[i][0] = csr_matrix((function_size, variable_sizes_0))
+
+        function_sizes_0 = function_sizes[0]
+        total_jacobian_0 = total_jacobian[0]
+        for j, variable_size in enumerate(variable_sizes):
+            total_jacobian_0[j] = csr_matrix((function_sizes_0, variable_size))
+
+        # Perform the assembly
+        jacobian_generator = self._get_jacobian_generator(
+            residuals, variables, is_residual=True # TODO: discuss
+        )
+
+        for jacobian, position in jacobian_generator:
+            # if isinstance(jacobian, JacobianOperator):
+            #     jacobian = jacobian.get_matrix_representation()
+
+            total_jacobian[position.row_index][position.column_index] = csr_matrix(
+                jacobian.real
+            )
+
+        return bmat(total_jacobian, format="csr")
+
+    # TODO: for m4
+    def _get_jacobian_generator(
+        self,
+        functions: Iterable[str],
+        variables: Iterable[str],
+        is_residual: bool = False,
+    ) -> Iterator[
+        tuple
+    ]:
+        """Iterate over Jacobian matrices.
+
+        Provide a generator to iterate over the Jacobians associated with each provided
+        pair (function, variable). The generator yields the Jacobian along with its
+        relative position in the to be assembled Jacobian.
+
+        Args:
+            functions: The functions to differentiate.
+            variables: The differentiation variables.
+            is_residual: Whether the functions are residuals.
+
+        Yields:
+            A tuple of the form (Jacobian, Position).
+        """
+        row = 0
+        # Iterate over outputs
+        for row_index, function in enumerate(functions):
+            column = 0
+            function_jacobian = self.disciplines[function].jac[function]
+            # Iterate over inputs
+            for column_index, variable in enumerate(variables):
+                jacobian = function_jacobian.get(variable, None)
+                variable_size = self.sizes[variable]
+
+                # If residual of the form Yi-Yi, then add -I to the Jacobian
+                if is_residual and function == variable:
+                    if jacobian is not None:
+                        # Make a copy to avoid in-place modifications
+                        jacobian_copy = jacobian.copy()
+
+                        if isinstance(jacobian_copy, ndarray):
+                            fill_diagonal(jacobian_copy, jacobian.diagonal() - 1)
+
+                        elif isinstance(jacobian_copy, sparse_classes):
+                            jacobian_copy.setdiag(jacobian.diagonal() - 1)
+
+                        # elif isinstance(jacobian_copy, JacobianOperator):
+                        #     jacobian_copy = jacobian_copy.shift_identity()
+
+                        jacobian = jacobian_copy
+
+                    else:
+                        jacobian = -eye(variable_size, dtype=int)
+
+                # Yield only if Jacobian exists
+                if jacobian is not None:
+                    yield (
+                        jacobian.real,
+                        self.JacobianPosition(
+                            row_slice=slice(row, row + jacobian.shape[0]),
+                            column_slice=slice(column, column + jacobian.shape[1]),
+                            row_index=row_index,
+                            column_index=column_index,
+                        ),
+                    )
+
+                column += variable_size
+            row += self.sizes[function]
 
     @profile
     def _dres_dvar_sparse_3_dok_orig(self, residuals, variables, n_residuals, n_variables):
@@ -277,7 +432,11 @@ class SoSJacobianAssembly(JacobianAssembly):
         :param transpose: if True, transpose the matrix
         """
         if matrix_type == JacobianAssembly.SPARSE:
-            if getenv("USE_PETSC", "").lower() in ("true", "1"):
+            if USE_M4_CSR:
+                sparse_dres_dvar = self._dres_dvar_sparse_4_csr_new(
+                    residuals, variables, n_residuals, n_variables
+                )
+            elif getenv("USE_PETSC", "").lower() in ("true", "1"):
                 sparse_dres_dvar = self._dres_dvar_sparse_1_lil_for_petsc(
                     residuals, variables, n_residuals, n_variables
                 )
