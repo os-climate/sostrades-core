b9b50df391e99b74ece89eddf0529afa438cd433
Vincent Drouet <vincent.drouet@capgemini.com>
2024-07-23 17:35:59 +0200

    refactor: formatting + update syntax

--- sostrades_core/tools/folder_operations.py
+++ sostrades_core/tools/folder_operations.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2024 Capgemini
+Copyright 2024 Capgemini.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,46 +13,61 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-import os
+
+from __future__ import annotations
+
 import shutil
 import time
+from pathlib import Path
 
 
-def rmtree_safe(path: str, retry: int = 100):
-    """
-    Remove a directory tree, but wait until it is really gone.
+def rmtree_safe(path: Path | str, retry: int = 100) -> None:
+    """Remove a directory tree, and wait for confirmation.
+
     Useful to fix some issues with shutil.rmtree with network drives.
 
-    Parameters:
-    path (str): The directory tree to remove.
-    retry (int): The maximum number of loop iterations to check if the directory is removed. Default is 100.
+    Args:
+        path: The directory tree to remove..
+        retry: The maximum number of loop iterations to check if the directory is removed.
+
+    Raises:
+        RuntimeError: If the removal fails after the maximum number of retries.
     """
+    path = Path(path).resolve()
     shutil.rmtree(path)
 
     attempts = 0
-    while os.path.exists(path) and attempts < retry:
+    while path.exists() and attempts < retry:
         time.sleep(0.05)
         attempts += 1
 
-    if os.path.exists(path):
-        raise Exception(f"Failed to remove the directory {path} after {retry} retries.")
+    if path.exists():
+        msg = f"Failed to remove the directory {path} after {retry} retries."
+        raise RuntimeError(msg)
 
 
-def makedirs_safe(name: str, mode: int = 511, exist_ok: bool = False, retry: int = 100):
+def makedirs_safe(name: str | Path, mode: int = 511, exist_ok: bool = False, retry: int = 100) -> None:
+    """Make a directory, but wait until it is really created.
+
+    Useful to fix some issues with Path.makedirs with network drives.
+
+    Args:
+        name: The directory tree to create.
+        mode: The mode of directory creation.
+        exist_ok: Whether to pass if the directory already exists. If False, will raise a FileExistsError.
+        retry: The maximum number of retries for creating the directory.
+
+    Raises:
+        RuntimeError: If the directory creation fails after the maximum number of retries.
     """
-    Makes a directory, but wait until it is really created.
-    Useful to fix some issues with os.makedirs with network drives.
-
-    Parameters:
-    name (str): The directory tree to create.
-    retry (int): The maximum number of loop iterations to check if the directory is created. Default is 100.
-    """
-    os.makedirs(name=name, mode=mode, exist_ok=exist_ok)
+    dir_path = Path(name).resolve()
+    dir_path.mkdir(mode=mode, exist_ok=exist_ok, parents=True)
 
     attempts = 0
-    while not os.path.exists(name) and attempts < retry:
+    while not dir_path.exists() and attempts < retry:
         time.sleep(0.05)
         attempts += 1
 
-    if not os.path.exists(name):
-        raise Exception(f"Failed to create the directory {name} after {retry} retries.")
+    if not dir_path.exists():
+        msg = f"Failed to create the directory {name} after {retry} retries."
+        raise RuntimeError(msg)
