bded7f4f8e223264895c16c0f4871083efbe2309
Vincent Drouet <vincent.drouet@capgemini.com>
2024-08-01 11:10:58 +0200

    Add headers

--- sostrades_core/sos_processes/test/test_sellar_uq/sellar_doe/__init__.py
+++ sostrades_core/sos_processes/test/test_sellar_uq/sellar_doe/__init__.py
@@ -1,9 +1,15 @@
-# Copyright (c) 2023 Capgemini Engineering
-# All rights reserved.
-#
-# Created on 09/07/2024, 15:26
-# Contributors:
-#    INITIAL AUTHORS - initial API and implementation and/or initial documentation
-#        :author:  Vincent Drouet
-#    OTHER AUTHORS   - MACROSCOPIC CHANGES
-"""Processes for sampling the Sellar MDA."""
+'''
+Copyright 2024 Capgemini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
