9d1a824b94e7c0bd0857ed186b1cdd587911bc5f
Valentin Joncquieres <valentin.joncquieres@capgemini.com>
2024-10-23 14:35:57 +0200

    add gemseo import

--- sostrades_core/__init__.py
+++ sostrades_core/__init__.py
@@ -26,9 +26,7 @@ parent_dir = dirname(__file__)
 GEMSEO_ADDON_DIR = "gemseo_addon"
 EXEC_ENGINE = "execution_engine"
 
+os.environ["GEMSEO_PATH"] = join(parent_dir, EXEC_ENGINE, GEMSEO_ADDON_DIR)
 # TODO hotfix for the gemseo circular import : first import gemseo to resolve base gemseo classes, then force refresh of factories
 # Should not be needed after a fix in gemseo code for factories
-# import gemseo
-
-os.environ["GEMSEO_PATH"] = join(parent_dir, EXEC_ENGINE, GEMSEO_ADDON_DIR)
-
+import gemseo
