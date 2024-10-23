877560add05eb833a7900aab1c8deda47d74b3e3
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2024-10-23 16:37:32 +0200

    ruff check disabled for hotfix

--- sostrades_core/__init__.py
+++ sostrades_core/__init__.py
@@ -29,4 +29,4 @@ EXEC_ENGINE = "execution_engine"
 os.environ["GEMSEO_PATH"] = join(parent_dir, EXEC_ENGINE, GEMSEO_ADDON_DIR)
 # TODO hotfix for the gemseo circular import : first import gemseo to resolve base gemseo classes, then force refresh of factories
 # Should not be needed after a fix in gemseo code for factories
-import gemseo
+import gemseo  # noqa: E402, F401
