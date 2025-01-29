9c6129cc8200cfe982454f9b017356ad1ccaa891
Vincent Drouet <vincent.drouet@capgemini.com>
2025-01-29 10:35:57 +0100

    feat: add monte carlo driver + proxy

--- sostrades_core/execution_engine/disciplines_wrappers/monte_carlo_driver_wrapper.py
+++ sostrades_core/execution_engine/disciplines_wrappers/monte_carlo_driver_wrapper.py
@@ -13,8 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+
 from __future__ import annotations
 
+from copy import deepcopy
+from enum import auto
 from typing import TYPE_CHECKING, Any, ClassVar
 
 from gemseo import create_scenario
@@ -22,11 +25,12 @@ from gemseo.algos.parameter_space import ParameterSpace
 from gemseo.settings.doe import PYDOE_LHS_Settings
 from gemseo.settings.formulations import DisciplinaryOpt_Settings
 from gemseo.uncertainty import create_statistics
-from numpy import atleast_1d, hstack
-from pandas import DataFrame, concat
-from strenum import StrEnum, auto
+from numpy import atleast_1d, cumsum, size, split, sqrt
+from pandas import concat
+from strenum import StrEnum
 
 from sostrades_core.execution_engine.disciplines_wrappers.driver_evaluator_wrapper import DriverEvaluatorWrapper
+from sostrades_core.execution_engine.proxy_driver_evaluator import ProxyDriverEvaluator
 
 DistributionsArgsType = dict[str, dict[str, Any]]
 
@@ -35,6 +39,41 @@ if TYPE_CHECKING:
     from gemseo.typing import NumberArray
 
     from sostrades_core.execution_engine.sos_discipline import SoSDiscipline
+
+
+class SoSInputNames(StrEnum):
+    """The names of the input parameters."""
+
+    batch_size = auto()
+    """The batch size for the sampling when using std or cv stopping criterion."""
+
+    input_distributions = auto()
+    """The distributions of each input variables."""
+
+    n_processes = auto()
+    """The number of processes for the sampling."""
+
+    n_samples = auto()
+    """The number of samples to evaluate."""
+
+    target_cv = auto()
+    """The target coefficients of variation for each component of the estimator."""
+
+    target_std = auto()
+    """The target standard deviations for each component of the estimator."""
+
+    wait_time_between_samples = auto()
+    """The time to wait between evaluationg two samples."""
+
+
+class SoSOutputNames(StrEnum):
+    """The names of the output parameters."""
+
+    input_samples = auto()
+    """The dictionary of input samples."""
+
+    output_samples = auto()
+    """The dictionary of output samples."""
 
 
 class StoppingCriterion(StrEnum):
@@ -58,11 +97,23 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
     The sampling is a Randomized Quasi Monte Carlo of the uncertain space.
     """
 
+    DISTRIBUTION_TYPE_KEY = "distribution_type"
+    """The key of the distribution dictionary containing the distribution type."""
+
+    SoSInputNames: ClassVar[type[SoSInputNames]] = SoSInputNames
+    """The names of the driver inputs passed by SoSTrades."""
+
+    SoSOutputNames: ClassVar[type[SoSOutputNames]] = SoSOutputNames
+    """The names of the driver outputs passed to SoSTrades."""
+
     StoppingCriterion: ClassVar[type[StoppingCriterion]] = StoppingCriterion
     """The available stopping criteria."""
 
     parameter_space: ParameterSpace
     """The parameter space containing the distributions used for sampling."""
+
+    outputs: list[str]
+    """The list of output variable full names."""
 
     _discipline: SoSDiscipline
     """The discipline or MDA used to evaluate the samples."""
@@ -78,7 +129,7 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
     """The target coefficients of variation for each component of the estimator."""
 
     _target_std: float | NumberArray | None
-    """The target standard variations for each component of the estimator."""
+    """The target standard deviations for each component of the estimator."""
 
     def _setup_discipline(self):
         """Get the evaluation discipline and set the default input values."""
@@ -86,36 +137,35 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
         self._init_input_data()
         self._discipline.default_input_data = self._get_input_data({})
 
-    def _setup_input_distributions(self) -> None:
-        """Setup the GEMSEO input distributions for sampling."""
+    def _setup_inputs_outputs(self) -> None:
+        """Setup the input distributions and output names."""
         self.parameter_space = ParameterSpace()
-
-        distributions_args: DistributionsArgsType = self.get_sosdisc_inputs["input_distributions"]
+        distributions_args: DistributionsArgsType = self.get_sosdisc_inputs(self.SoSInputNames.input_distributions)
         for var_name, distr_args in distributions_args.items():
-            distributions_type = distr_args.pop("distribution_type")
+            # Copy the dict as it will be modified
+            distr_args = deepcopy(distr_args)
+            distributions_type = distr_args.pop(self.DISTRIBUTION_TYPE_KEY)
             self.parameter_space.add_random_variable(
                 name=var_name,
                 distribution=distributions_type,
-                **distributions_args,
+                **distr_args,
             )
+
+        gather_outputs = self.get_sosdisc_inputs(ProxyDriverEvaluator.GATHER_OUTPUTS)
+        short_output_names = list(gather_outputs[gather_outputs["selected_output"]]["full_name"])
+        self.outputs = [f"{self.attributes['driver_name']}.{name}" for name in short_output_names]
 
     def _setup_stopping_criterion(self) -> None:
         """Setup the stopping criterion."""
-        self._n_samples = None
-        self._target_cv = None
-        self._target_std = None
-        if n_samples := self.get_sosdisc_inputs("n_samples") is not None:
-            self._n_samples = n_samples
-            self._stopping_criterion = StoppingCriterion.n_samples
-        elif target_std := self.get_sosdisc_inputs("target_std") is not None:
+        self._n_samples = self.get_sosdisc_inputs(self.SoSInputNames.n_samples)
+        if target_std := self.get_sosdisc_inputs(self.SoSInputNames.target_std):
             self._target_std = atleast_1d(target_std).flatten()
             self._stopping_criterion = StoppingCriterion.std
-        elif target_cv := self.get_sosdisc_inputs("target_cv") is not None:
+        elif target_cv := self.get_sosdisc_inputs(self.SoSInputNames.target_cv):
             self._target_cv = atleast_1d(target_cv).flatten()
             self._stopping_criterion = StoppingCriterion.cv
         else:
-            msg = f"Monte Carlo sampling requires a stopping criterion (`{StoppingCriterion.n_samples}`, `{StoppingCriterion.std}` or {StoppingCriterion.cv}`)."
-            raise ValueError(msg)
+            self._stopping_criterion = StoppingCriterion.n_samples
 
     def _evaluate_n_samples(self, n_samples: int) -> Dataset:
         """Sample the input distributions for a given number of samples and evaluate the outputs.
@@ -126,21 +176,23 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
         Returns:
             The dataset of input and output values.
         """
-        inputs = self.parameter_space.variable_names
-        outputs = self.attributes["eval_out_list"]
         formulation_settings = DisciplinaryOpt_Settings()
         mc_scenario = create_scenario(
             disciplines=[self._discipline],
-            objective_name=outputs,
-            design_space=ParameterSpace,
+            objective_name=self.outputs,
+            design_space=self.parameter_space,
             scenario_type="DOE",
             formulation_settings_model=formulation_settings,
         )
-        n_processes = self.get_sosdisc_inputs("n_processes") or 1
-        algo_settings = PYDOE_LHS_Settings(n_samples=n_samples, n_processes=n_processes)
+        n_processes = self.get_sosdisc_inputs(self.SoSInputNames.n_processes)
+        wait_time_between_samples = self.get_sosdisc_inputs(self.SoSInputNames.wait_time_between_samples)
+        algo_settings = PYDOE_LHS_Settings(
+            n_samples=n_samples,
+            n_processes=n_processes,
+            wait_time_between_samples=wait_time_between_samples,
+        )
         mc_scenario.execute(algo_name="PYDOE_LHS", algo_settings_model=algo_settings)
-        # Keep only the request inputs and outputs
-        return mc_scenario.to_dataset().loc[:, (slice(None), inputs + outputs)]
+        return mc_scenario.to_dataset()
 
     def _is_criterion_reached(self, dataset: Dataset) -> bool:
         """Check whether the stopping criterion is reached.
@@ -151,12 +203,13 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
         Returns:
             Whether the criterion is reached.
         """
-        analysis = create_statistics(dataset, variable_names=self.get_sosdisc_inputs["eval_out_list"])
+        n = dataset.shape[0]
+        analysis = create_statistics(dataset, variable_names=["_".join(self.outputs)])
         if self._stopping_criterion == StoppingCriterion.std:
-            std = hstack(tuple(analysis.compute_standard_deviation()))
-            return all(std <= self._target_std)
-        cv = hstack(tuple(analysis.compute_variation_coefficient()))
-        return all(cv <= self._target_std)
+            std = next(iter(analysis.compute_standard_deviation().values()))
+            return all(std / sqrt(n) <= self._target_std)
+        cv = next(iter(analysis.compute_variation_coefficient().values()))
+        return all(cv / sqrt(n) <= self._target_cv)
 
     def _compute_samples(self) -> Dataset:
         """Compute the Monte Carlo samples until the stopping criterion is reached.
@@ -167,7 +220,7 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
         if self._stopping_criterion == StoppingCriterion.n_samples:
             return self._evaluate_n_samples(self._n_samples)
 
-        batch_size = self.get_sosdisc_inputs("batch_size") or self._n_samples // 10
+        batch_size = self.get_sosdisc_inputs(self.SoSInputNames.batch_size) or self._n_samples // 100
         dataset = self._evaluate_n_samples(batch_size)
         while not self._is_criterion_reached(dataset) and dataset.shape[0] < self._n_samples:
             dataset = concat((dataset, self._evaluate_n_samples(batch_size)))
@@ -176,12 +229,22 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
     def _process_outputs(self, dataset: Dataset) -> None:
         """Process and export the input and output values of the sampling.
 
+        The output samples are gathered in a single array.
+        We need to retrieve the size of each separate output
+        and split the array into the sub-array corresponding to each output.
+
         Args:
             dataset: The dataset containing the input and output values.
         """
-        dict_of_arrays = dataset.to_dict_of_arrays()
-        self.store_sos_outputs_values({"samples_inputs_df": DataFrame(dict_of_arrays["designs"])})
-        self.store_sos_outputs_values({"samples_outputs_df": DataFrame(dict_of_arrays["functions"])})
+        samples_dict = dataset.to_dict_of_arrays()
+        input_samples = samples_dict["designs"]
+        self.store_sos_outputs_values({self.SoSOutputNames.input_samples: input_samples})
+
+        output_sizes = [size(self._discipline.local_data[output]) for output in self.outputs]
+        output_array = next(iter(samples_dict["functions"].values()))
+        output_arrays = split(output_array, cumsum(output_sizes), axis=1)
+        output_samples = {output: output_arrays[i] for i, output in enumerate(self.outputs)}
+        self.store_sos_outputs_values({self.SoSOutputNames.output_samples: output_samples})
 
     def run(self) -> None:
         """Perform a Quasi Monte Carlo sampling of the selected output(s).
@@ -192,7 +255,7 @@ class MonteCarloDriverWrapper(DriverEvaluatorWrapper):
         """
         # Setup the run
         self._setup_discipline()
-        self._setup_input_distributions()
+        self._setup_inputs_outputs()
         self._setup_stopping_criterion()
 
         dataset = self._compute_samples()
---
+++ sostrades_core/execution_engine/proxy_monte_carlo_driver.py
@@ -0,0 +1,108 @@
+'''
+Copyright 2025 Capgemini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+from __future__ import annotations
+
+from typing import Any, ClassVar
+
+from sostrades_core.execution_engine.disciplines_wrappers.monte_carlo_driver_wrapper import MonteCarloDriverWrapper
+from sostrades_core.execution_engine.proxy_driver_evaluator import ProxyDriverEvaluator
+from sostrades_core.execution_engine.proxy_mono_instance_driver import ProxyMonoInstanceDriver
+
+
+class ProxyMonteCarloDriver(ProxyMonoInstanceDriver):
+    """The proxy for the Monte Carlo driver."""
+
+    _ontology_data: ClassVar = {
+        'label': 'Monte Carlo Driver',
+        'type': 'Research',
+        'source': 'SoSTrades Project',
+        'validated': '',
+        'validated_by': 'SoSTrades Project',
+        'last_modification_date': '',
+        'category': '',
+        'definition': '',
+        'icon': '',
+        'version': '',
+    }
+
+    REF_DISCIPLINE_NAME: str = "Eval_MC"
+
+    DESC_IN: ClassVar[dict[str, Any]] = {
+        ProxyDriverEvaluator.GATHER_OUTPUTS: {
+            ProxyDriverEvaluator.TYPE: 'dataframe',
+            ProxyDriverEvaluator.DATAFRAME_DESCRIPTOR: {
+                'selected_output': ('bool', None, True),
+                'full_name': ('string', None, False),
+                'output_name': ('multiple', None, True),
+            },
+            ProxyDriverEvaluator.DATAFRAME_EDITION_LOCKED: False,
+            ProxyDriverEvaluator.STRUCTURING: True,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.batch_size: {
+            ProxyDriverEvaluator.TYPE: 'int',
+            ProxyDriverEvaluator.DEFAULT: 0,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.input_distributions: {
+            ProxyDriverEvaluator.TYPE: 'dict',
+        },
+        MonteCarloDriverWrapper.SoSInputNames.n_processes: {
+            ProxyDriverEvaluator.TYPE: 'int',
+            ProxyDriverEvaluator.DEFAULT: 1,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.n_samples: {
+            ProxyDriverEvaluator.TYPE: 'int',
+            ProxyDriverEvaluator.DEFAULT: 1000,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.target_cv: {
+            ProxyDriverEvaluator.TYPE: 'float',
+            ProxyDriverEvaluator.DEFAULT: 0,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.target_std: {
+            ProxyDriverEvaluator.TYPE: 'float',
+            ProxyDriverEvaluator.DEFAULT: 0,
+        },
+        MonteCarloDriverWrapper.SoSInputNames.wait_time_between_samples: {
+            ProxyDriverEvaluator.TYPE: 'float',
+            ProxyDriverEvaluator.DEFAULT: 0.0,
+        },
+    }
+
+    DESC_OUT: ClassVar[dict[str, Any]] = {
+        MonteCarloDriverWrapper.SoSOutputNames.input_samples: {
+            ProxyDriverEvaluator.TYPE: 'dict',
+            'unit': None,
+        },
+        MonteCarloDriverWrapper.SoSOutputNames.output_samples: {
+            ProxyDriverEvaluator.TYPE: 'dict',
+            'unit': None,
+        },
+    }
+
+    def update_data_io_with_subprocess_io(self):
+        """
+        Update the DriverEvaluator _data_in with subprocess i/o.
+        Overload the ProxyDriverEvaluator method to avoid adding the subprocess outputs.
+        """
+        self._restart_data_io_to_disc_io()
+        for proxy_disc in self.proxy_disciplines:
+            subprocess_data_in = proxy_disc.get_data_io_with_full_name(self.IO_TYPE_IN, as_namespaced_tuple=True)
+            self.update_data_io_with_child(subprocess_data_in, {})
+
+    def setup_sos_disciplines(self) -> None:
+        """Overload the ProxyMonoInstanceDriver method to avoid adding irrelevant outputs."""
+        # super().setup_sos_disciplines()
+        # self.build_simple_data_io(self.IO_TYPE_OUT)
+        # self.clean_variables(self.eval_out_names, self.IO_TYPE_OUT)
