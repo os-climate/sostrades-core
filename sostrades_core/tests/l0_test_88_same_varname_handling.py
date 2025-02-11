dfc5c938d64c393e62e2b349c49d2a9856f7405d
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2025-02-11 17:48:44 +0100

    add commented-out _test to manifest an incoherence of coupling io construction only affecting weak couplings declared different-name (same-value) namespaces

--- sostrades_core/tests/l0_test_88_same_varname_handling.py
+++ sostrades_core/tests/l0_test_88_same_varname_handling.py
@@ -72,6 +72,37 @@ class TestSameVarnameHandling(unittest.TestCase):
         self.assertEqual(true_x, true_x_out)
         self.assertTrue(test_a)
 
+    def _test_02_mda_io_with_namespaced_tuples(self):
+        """
+        Test the proper construction of coupling IO in the presence of weak couplings that come from namespaces which
+        have different name but same value.
+        """
+        exec_eng = ExecutionEngine(self.name)
+        # the two disciplines built below are weakly coupled through variable y declared in different-name namespaces
+        disc_dir = 'sostrades_core.sos_wrapping.test_discs.'
+        disc1_builder = exec_eng.factory.get_builder_from_module("Disc1", disc_dir + 'disc1.Disc1')
+        disc2_builder = exec_eng.factory.get_builder_from_module("Disc2", disc_dir + 'disc2b.Disc2')
+        # set two namespaces to same value as to make the weak coupling effective
+        ns_dict = {'ns_ac': exec_eng.study_name, "ns_other": exec_eng.study_name}
+        exec_eng.ns_manager.add_ns_def(ns_dict)
+        exec_eng.factory.set_builders_to_coupling_builder([disc1_builder, disc2_builder])
+        private_values = {
+            self.name + '.x': 10.,
+            self.name + '.Disc1.a': 5.,
+            self.name + '.Disc1.b': 25431.,
+            self.name + '.y': 4.,
+            self.name + '.Disc2.constant': 3.1416,
+            self.name + '.Disc2.power': 2}
+        exec_eng.configure()
+        exec_eng.load_study_from_input_dict(private_values)
+        exec_eng.prepare_execution()
+
+        # the weak coupling should be an output and not an input of the process on both GEMSEO and Proxy sides
+        self.assertIn(f"{self.name}.y", exec_eng.root_process.discipline_wrapp.discipline.get_output_data_names())
+        self.assertNotIn(f"{self.name}.y", exec_eng.root_process.discipline_wrapp.discipline.get_input_data_names())
+        self.assertIn(f"{self.name}.y", exec_eng.root_process.get_output_data_names())
+        self.assertNotIn(f"{self.name}.y", exec_eng.root_process.get_input_data_names())
+
 
 if '__main__' == __name__:
     cls = TestSameVarnameHandling()
