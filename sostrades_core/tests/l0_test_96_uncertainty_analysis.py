b07fa5228c4cf9d33e1ce802da8c528a164b558b
Vincent Drouet <vincent.drouet@capgemini.com>
2025-02-18 11:52:03 +0100

    test: fix test for uncertainty analysis

--- sostrades_core/tests/l0_test_96_uncertainty_analysis.py
+++ sostrades_core/tests/l0_test_96_uncertainty_analysis.py
@@ -20,40 +20,40 @@ from pathlib import Path
 
 import pytest
 from numpy import array
-from pandas import read_csv
-from pandas.testing import assert_frame_equal
+from numpy.testing import assert_allclose
+from pandas import DataFrame, read_csv
 
 from sostrades_core.sos_processes.test.test_uncertainty_analysis.usecase import Study
 from sostrades_core.sos_wrapping.analysis_discs.uncertainty_analysis import UncertaintyAnalysis
 
-STUDY_NAME = "MC"
 POST_NAME = "MC post"
+EVAL_MC_NAME = "Eval_MC"
 
 
 @pytest.mark.parametrize("n_objectives", [1, 5])
-def test_uncertainty_analysis(n_objective: int):
+def test_uncertainty_analysis(n_objectives: int):
     """Test the UncertaintyAnalysis discipline with fixed input-output samples."""
     input_samples = read_csv(Path(__file__).parent / "data/test_96_input_samples.csv")
     output_samples = read_csv(Path(__file__).parent / "data/test_96_output_samples.csv")
     expected_stats = read_csv(Path(__file__).parent / "data/test_96_statistics.csv")
-    if n_objective == 1:
-        output_samples = output_samples["usecase.Eval_MC.obj"]
-        expected_stats = expected_stats["usecase.Eval_MC.obj"]
-        threshold = 45
+    if n_objectives == 1:
+        output_samples = DataFrame(output_samples["usecase.Eval_MC.obj"])
+        expected_stats = expected_stats.loc[:, ["usecase.Eval_MC.x", "usecase.Eval_MC.obj"]]
+        threshold = array([5, 45])
     else:
         threshold = array([5, 0, -19.5, 45, 5, 4])
-    input_dict = {
-        f"{STUDY_NAME}.{POST_NAME}.{UncertaintyAnalysis.SoSInputNames.INPUT_SAMPLES}": input_samples,
-        f"{STUDY_NAME}.{POST_NAME}.{UncertaintyAnalysis.SoSInputNames.OUTPUT_SAMPLES}": output_samples,
-        f"{STUDY_NAME}.{POST_NAME}.{UncertaintyAnalysis.SoSInputNames.PROBABILITY_THRESHOLD}": threshold,
-    }
     study = Study(run_usecase=True)
+    input_dict = {
+        f"{study.study_name}.{EVAL_MC_NAME}.{UncertaintyAnalysis.SoSInputNames.INPUT_SAMPLES}": input_samples,
+        f"{study.study_name}.{EVAL_MC_NAME}.{UncertaintyAnalysis.SoSInputNames.OUTPUT_SAMPLES}": output_samples,
+        f"{study.study_name}.{POST_NAME}.{UncertaintyAnalysis.SoSInputNames.PROBABILITY_THRESHOLD}": threshold,
+    }
     study.load_data(from_input_dict=input_dict)
     study.run()
     # Check the output
     dm = study.execution_engine.dm
-    stats = dm.get_value(f"{STUDY_NAME}.{POST_NAME}.{UncertaintyAnalysis.SoSOutputNames.STATISTICS}")
-    assert_frame_equal(stats, expected_stats)
+    stats = dm.get_value(f"{study.study_name}.{POST_NAME}.{UncertaintyAnalysis.SoSOutputNames.STATISTICS}")
+    assert_allclose(stats.to_numpy(), expected_stats.to_numpy())
 
 
 if __name__ == "__main__":
