a2decffd2ea83cf5d28dddc93586c7c2ce589c36
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2025-02-20 15:31:14 +0100

    proposal to avoid debug errors with petsc-dependant imports consisting in conditional definition of classes concerned

--- sostrades_core/execution_engine/gemseo_addon/linear_solvers/ksp_lib.py
+++ sostrades_core/execution_engine/gemseo_addon/linear_solvers/ksp_lib.py
@@ -16,245 +16,247 @@ limitations under the License.
 '''
 
 from __future__ import annotations
+from os import getenv
 
-import logging
-from typing import TYPE_CHECKING, Any, ClassVar
+if getenv("USE_PETSC", "").lower() in ("true", "1"):
+    import logging
+    from typing import TYPE_CHECKING, Any, ClassVar
 
-import petsc4py
-from gemseo.algos.linear_solvers.base_linear_solver_library import (
-    BaseLinearSolverLibrary,
-    LinearSolverDescription,
-)
-from gemseo_petsc.linear_solvers.petsc_ksp import (
-    _convert_ndarray_to_mat_or_vec,
-)
-from numpy import isnan, ndarray
+    import petsc4py
+    from gemseo.algos.linear_solvers.base_linear_solver_library import (
+        BaseLinearSolverLibrary,
+        LinearSolverDescription,
+    )
+    from gemseo_petsc.linear_solvers.petsc_ksp import (
+        _convert_ndarray_to_mat_or_vec,
+    )
+    from numpy import isnan, ndarray
 
-from sostrades_core.execution_engine.gemseo_addon.linear_solvers.settings.petsc_ksp_settings import (
-    BaseSoSPetscKSPSettings,
-)
+    from sostrades_core.execution_engine.gemseo_addon.linear_solvers.settings.petsc_ksp_settings import (
+        BaseSoSPetscKSPSettings,
+    )
 
-# Must be done before from petsc4py import PETSc, this loads the options from command args in the options database.
-petsc4py.init([])
-from petsc4py import PETSc  # noqa: E402
+    # Must be done before from petsc4py import PETSc, this loads the options from command args in the options database.
+    petsc4py.init([])
+    from petsc4py import PETSc  # noqa: E402
 
-if TYPE_CHECKING:
-    from gemseo.algos.linear_solvers.linear_problem import LinearProblem
-    from gemseo.typing import NumberArray
+    if TYPE_CHECKING:
+        from gemseo.algos.linear_solvers.linear_problem import LinearProblem
+        from gemseo.typing import NumberArray
 
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-KSP_CONVERGED_REASON = {
-    1: 'KSP_CONVERGED_RTOL_NORMAL',
-    9: 'KSP_CONVERGED_ATOL_NORMAL',
-    2: 'KSP_CONVERGED_RTOL',
-    3: 'KSP_CONVERGED_ATOL',
-    4: 'KSP_CONVERGED_ITS',
-    5: 'KSP_CONVERGED_CG_NEG_CURVE',
-    6: 'KSP_CONVERGED_CG_CONSTRAINED',
-    7: 'KSP_CONVERGED_STEP_LENGTH',
-    8: 'KSP_CONVERGED_HAPPY_BREAKDOWN',
-    -2: 'KSP_DIVERGED_NULL',
-    -3: 'KSP_DIVERGED_ITS',
-    -4: 'KSP_DIVERGED_DTOL',
-    -5: 'KSP_DIVERGED_BREAKDOWN',
-    -6: 'KSP_DIVERGED_BREAKDOWN_BICG',
-    -7: 'KSP_DIVERGED_NONSYMMETRIC',
-    -8: 'KSP_DIVERGED_INDEFINITE_PC',
-    -9: 'KSP_DIVERGED_NANORINF',
-    -10: 'KSP_DIVERGED_INDEFINITE_MAT',
-    -11: 'KSP_DIVERGED_PC_FAILED',
-    0: 'KSP_CONVERGED_ITERATING',
-}
-
-
-class SoSPetscKSPAlgos(BaseLinearSolverLibrary):
-    """Interface to PETSC KSP.
-
-    For further information, please read
-    https://petsc4py.readthedocs.io/en/stable/manual/ksp/
-    https://petsc.org/release/docs/manualpages/KSP/KSP.html#KSP
-    https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.KSP.Type-class.html
-
-    KSP example here:
-    https://fossies.org/linux/petsc/src/binding/petsc4py/demo/petsc-examples/ksp/ex2.py
-    """
-
-    AVAILABLE_LINEAR_SOLVERS: tuple[str] = ('GMRES', 'LGMRES', 'BICG', 'BCGS')
-    """The available linear solvers."""
-
-    AVAILABLE_PRECONDITIONERS: tuple[str] = ('jacobi', 'ilu', 'gasm')
-    """The available preconditioners."""
-
-    ALGORITHM_INFOS: ClassVar[dict[str, LinearSolverDescription]] = {
-        f"{solver_name}_PETSC": LinearSolverDescription(
-            algorithm_name=solver_name,
-            description=f"Linear solver {solver_name}",
-            internal_algorithm_name=solver_name.lower(),
-            lhs_must_be_linear_operator=True,
-            library_name="PETSC_KSP",
-            website="https://petsc.org/release/docs/manualpages/KSP/KSP.html#KSP",
-            Settings=type(
-                f"Petsc{solver_name}Settings",
-                (BaseSoSPetscKSPSettings,),
-                {"_TARGET_CLASS_NAME": f"{solver_name}_PETSC"},
-            ),
-        )
-        for solver_name in AVAILABLE_LINEAR_SOLVERS
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.setLevel(logging.DEBUG)
+    KSP_CONVERGED_REASON = {
+        1: 'KSP_CONVERGED_RTOL_NORMAL',
+        9: 'KSP_CONVERGED_ATOL_NORMAL',
+        2: 'KSP_CONVERGED_RTOL',
+        3: 'KSP_CONVERGED_ATOL',
+        4: 'KSP_CONVERGED_ITS',
+        5: 'KSP_CONVERGED_CG_NEG_CURVE',
+        6: 'KSP_CONVERGED_CG_CONSTRAINED',
+        7: 'KSP_CONVERGED_STEP_LENGTH',
+        8: 'KSP_CONVERGED_HAPPY_BREAKDOWN',
+        -2: 'KSP_DIVERGED_NULL',
+        -3: 'KSP_DIVERGED_ITS',
+        -4: 'KSP_DIVERGED_DTOL',
+        -5: 'KSP_DIVERGED_BREAKDOWN',
+        -6: 'KSP_DIVERGED_BREAKDOWN_BICG',
+        -7: 'KSP_DIVERGED_NONSYMMETRIC',
+        -8: 'KSP_DIVERGED_INDEFINITE_PC',
+        -9: 'KSP_DIVERGED_NANORINF',
+        -10: 'KSP_DIVERGED_INDEFINITE_MAT',
+        -11: 'KSP_DIVERGED_PC_FAILED',
+        0: 'KSP_CONVERGED_ITERATING',
     }
 
-    def _run(self, problem: LinearProblem, **settings: Any) -> ndarray:
-        """Run the algorithm.
 
-        Args:
-            problem: The linear problem to solve.
-            **settings: The algorithm settings.
+    class SoSPetscKSPAlgos(BaseLinearSolverLibrary):
+        """Interface to PETSC KSP.
 
-        Returns:
-            The solution of the problem.
+        For further information, please read
+        https://petsc4py.readthedocs.io/en/stable/manual/ksp/
+        https://petsc.org/release/docs/manualpages/KSP/KSP.html#KSP
+        https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.KSP.Type-class.html
+
+        KSP example here:
+        https://fossies.org/linux/petsc/src/binding/petsc4py/demo/petsc-examples/ksp/ex2.py
         """
-        # Set default settings
-        # TODO: move to main linear solver classes so that all solvers benefit from these default inputs definition
-        solver = self.ALGORITHM_INFOS[self.algo_name].internal_algorithm_name
-        b = problem.rhs
-        a = problem.lhs
-        if 'maxiter' not in settings:
-            settings['maxiter'] = 50 * b.shape[0]
-        else:
-            settings['maxiter'] = min(settings['maxiter'], 50 * a.shape[0])
 
-        # first run
-        settings["old_sol"] = None
+        AVAILABLE_LINEAR_SOLVERS: tuple[str] = ('GMRES', 'LGMRES', 'BICG', 'BCGS')
+        """The available linear solvers."""
 
-        sol, info, ksp = self._run_petsc_strategy(problem, solver, **settings)
+        AVAILABLE_PRECONDITIONERS: tuple[str] = ('jacobi', 'ilu', 'gasm')
+        """The available preconditioners."""
 
-        if info < 0:
-            settings['preconditioner_type'] = 'gasm'
-            settings['old_sol'] = sol
+        ALGORITHM_INFOS: ClassVar[dict[str, LinearSolverDescription]] = {
+            f"{solver_name}_PETSC": LinearSolverDescription(
+                algorithm_name=solver_name,
+                description=f"Linear solver {solver_name}",
+                internal_algorithm_name=solver_name.lower(),
+                lhs_must_be_linear_operator=True,
+                library_name="PETSC_KSP",
+                website="https://petsc.org/release/docs/manualpages/KSP/KSP.html#KSP",
+                Settings=type(
+                    f"Petsc{solver_name}Settings",
+                    (BaseSoSPetscKSPSettings,),
+                    {"_TARGET_CLASS_NAME": f"{solver_name}_PETSC"},
+                ),
+            )
+            for solver_name in AVAILABLE_LINEAR_SOLVERS
+        }
 
-            # second run with bcgs
-            sol, info, ksp = self._run_petsc_strategy(problem, "bcgs", **settings)
-            if info >= 0:
-                LOGGER.warning(
-                    "The second try with GASM preconditioner and bi CG stabilized linear solver has converged at %s",
-                    ksp.getResidualNorm(),
-                )
-            elif info == -3:
-                # DIVERGED_ITS
-                LOGGER.warning(
-                    "DIVERGED_ITS error : the number of iterations of the solver is %s with a max iter of %s, running again with 10*maxiter",
-                    len(ksp.getConvergenceHistory()),
-                    settings['maxiter'],
-                )
-                settings['maxiter'] = 10 * settings['maxiter']
+        def _run(self, problem: LinearProblem, **settings: Any) -> ndarray:
+            """Run the algorithm.
+
+            Args:
+                problem: The linear problem to solve.
+                **settings: The algorithm settings.
+
+            Returns:
+                The solution of the problem.
+            """
+            # Set default settings
+            # TODO: move to main linear solver classes so that all solvers benefit from these default inputs definition
+            solver = self.ALGORITHM_INFOS[self.algo_name].internal_algorithm_name
+            b = problem.rhs
+            a = problem.lhs
+            if 'maxiter' not in settings:
+                settings['maxiter'] = 50 * b.shape[0]
+            else:
+                settings['maxiter'] = min(settings['maxiter'], 50 * a.shape[0])
+
+            # first run
+            settings["old_sol"] = None
+
+            sol, info, ksp = self._run_petsc_strategy(problem, solver, **settings)
+
+            if info < 0:
                 settings['preconditioner_type'] = 'gasm'
                 settings['old_sol'] = sol
 
-                # third run with bcgs and larger maxiter
+                # second run with bcgs
                 sol, info, ksp = self._run_petsc_strategy(problem, "bcgs", **settings)
+                if info >= 0:
+                    LOGGER.warning(
+                        "The second try with GASM preconditioner and bi CG stabilized linear solver has converged at %s",
+                        ksp.getResidualNorm(),
+                    )
+                elif info == -3:
+                    # DIVERGED_ITS
+                    LOGGER.warning(
+                        "DIVERGED_ITS error : the number of iterations of the solver is %s with a max iter of %s, running again with 10*maxiter",
+                        len(ksp.getConvergenceHistory()),
+                        settings['maxiter'],
+                    )
+                    settings['maxiter'] = 10 * settings['maxiter']
+                    settings['preconditioner_type'] = 'gasm'
+                    settings['old_sol'] = sol
 
-        return problem.solution
+                    # third run with bcgs and larger maxiter
+                    sol, info, ksp = self._run_petsc_strategy(problem, "bcgs", **settings)
 
-    def _run_petsc_strategy(
-        self, problem: LinearProblem, solver: str, **settings
-    ) -> tuple[NumberArray | None, int, Any]:
-        """Runs the solver.
+            return problem.solution
 
-        Args:
-            problem: The linear algebra problem to solve.
-            solver: The solver name.
-            **settings: The solver settings.
+        def _run_petsc_strategy(
+            self, problem: LinearProblem, solver: str, **settings
+        ) -> tuple[NumberArray | None, int, Any]:
+            """Runs the solver.
 
-        Returns:
-            A tuple containing:
-              - the solution of the problem.
-              - the return code.
-              - the KSP problem.
-        """
-        # Initialize the KSP solver.
-        options_cmd = settings.get("options_cmd")
-        if options_cmd is not None:
-            petsc4py.init(options_cmd)
-        else:
-            petsc4py.init()
+            Args:
+                problem: The linear algebra problem to solve.
+                solver: The solver name.
+                **settings: The solver settings.
 
-        # Create the solver
-        ksp = PETSc.KSP().create()
-        # Set all solver settings
-        ksp.setType(solver)
-        ksp.setTolerances(settings["rtol"], settings["atol"], settings["dtol"], settings["maxiter"])
-        ksp.setConvergenceHistory()
-
-        b = problem.rhs
-        a = problem.lhs
-
-        # CHeck Nan in matrix
-        if isnan(a.min()):
-            LOGGER.error('The matrix A in the linear solver contains a NaN')
-        # Transform sparse matrix into petsc matrix
-        a_mat = _convert_ndarray_to_mat_or_vec(a)
-        # Set the matrix in the solver
-        ksp.setOperators(a_mat)
-        # the chosen preconditioner
-        prec_type = settings.get("preconditioner_type")
-        if prec_type is not None:
-            pc = ksp.getPC()
-            pc.setType(prec_type)
-            pc.setUp()
-
-        # transform the b array in petsc vector
-        b_mat = _convert_ndarray_to_mat_or_vec(b)
-        # Use b as first solution (same size)
-        old_sol = settings.get("old_sol")
-        if old_sol is not None:
-            solution = _convert_ndarray_to_mat_or_vec(old_sol)
-        else:
-            solution = b_mat.duplicate()
-            solution.set(0)
-        if settings["view_config"]:
-            ksp.view()
-        # Solve the ksp petsc solver
-        ksp.solve(b_mat, solution)
-        problem.solution = solution.getArray().copy()  # added a copy() like in GEMSEO
-        problem.convergence_info = ksp.reason
-
-        # Reason to positive for convergence, 0 for no convergence, and negative for failure to converge
-        if problem.convergence_info > 0:
-            info = 0
-            if problem.convergence_info in (1, 2):
-                LOGGER.warning(
-                    'The PETSc linear solver has converged with relative tolerance %s, the final residual norm is %s ; check your linear problem',
-                    settings["rtol"],
-                    ksp.getResidualNorm(),
-                )
-            elif problem.convergence_info == 4:
-                LOGGER.warning(
-                    'The PETSc linear solver has converged after max iterations %s, the final residual norm is %s ; check your linear problem',
-                    settings["maxiter"],
-                    ksp.getResidualNorm(),
-                )
-            elif problem.convergence_info in (3, 9):
-                pass
+            Returns:
+                A tuple containing:
+                  - the solution of the problem.
+                  - the return code.
+                  - the KSP problem.
+            """
+            # Initialize the KSP solver.
+            options_cmd = settings.get("options_cmd")
+            if options_cmd is not None:
+                petsc4py.init(options_cmd)
             else:
+                petsc4py.init()
+
+            # Create the solver
+            ksp = PETSc.KSP().create()
+            # Set all solver settings
+            ksp.setType(solver)
+            ksp.setTolerances(settings["rtol"], settings["atol"], settings["dtol"], settings["maxiter"])
+            ksp.setConvergenceHistory()
+
+            b = problem.rhs
+            a = problem.lhs
+
+            # CHeck Nan in matrix
+            if isnan(a.min()):
+                LOGGER.error('The matrix A in the linear solver contains a NaN')
+            # Transform sparse matrix into petsc matrix
+            a_mat = _convert_ndarray_to_mat_or_vec(a)
+            # Set the matrix in the solver
+            ksp.setOperators(a_mat)
+            # the chosen preconditioner
+            prec_type = settings.get("preconditioner_type")
+            if prec_type is not None:
+                pc = ksp.getPC()
+                pc.setType(prec_type)
+                pc.setUp()
+
+            # transform the b array in petsc vector
+            b_mat = _convert_ndarray_to_mat_or_vec(b)
+            # Use b as first solution (same size)
+            old_sol = settings.get("old_sol")
+            if old_sol is not None:
+                solution = _convert_ndarray_to_mat_or_vec(old_sol)
+            else:
+                solution = b_mat.duplicate()
+                solution.set(0)
+            if settings["view_config"]:
+                ksp.view()
+            # Solve the ksp petsc solver
+            ksp.solve(b_mat, solution)
+            problem.solution = solution.getArray().copy()  # added a copy() like in GEMSEO
+            problem.convergence_info = ksp.reason
+
+            # Reason to positive for convergence, 0 for no convergence, and negative for failure to converge
+            if problem.convergence_info > 0:
+                info = 0
+                if problem.convergence_info in (1, 2):
+                    LOGGER.warning(
+                        'The PETSc linear solver has converged with relative tolerance %s, the final residual norm is %s ; check your linear problem',
+                        settings["rtol"],
+                        ksp.getResidualNorm(),
+                    )
+                elif problem.convergence_info == 4:
+                    LOGGER.warning(
+                        'The PETSc linear solver has converged after max iterations %s, the final residual norm is %s ; check your linear problem',
+                        settings["maxiter"],
+                        ksp.getResidualNorm(),
+                    )
+                elif problem.convergence_info in (3, 9):
+                    pass
+                else:
+                    LOGGER.warning(
+                        'The PETSc linear solver has converged with %s, the tolerance is %s, the final residual norm is %s ; check your linear problem',
+                        KSP_CONVERGED_REASON[problem.convergence_info],
+                        settings["atol"],
+                        ksp.getResidualNorm(),
+                    )
+            elif problem.convergence_info == 0:
+                info = 1
+            else:
+                info = problem.convergence_info
                 LOGGER.warning(
-                    'The PETSc linear solver has converged with %s, the tolerance is %s, the final residual norm is %s ; check your linear problem',
+                    'The PETSc linear solver has not converged with error %s, the final residual norm is %s ; check your linear problem',
                     KSP_CONVERGED_REASON[problem.convergence_info],
-                    settings["atol"],
                     ksp.getResidualNorm(),
                 )
-        elif problem.convergence_info == 0:
-            info = 1
-        else:
-            info = problem.convergence_info
-            LOGGER.warning(
-                'The PETSc linear solver has not converged with error %s, the final residual norm is %s ; check your linear problem',
-                KSP_CONVERGED_REASON[problem.convergence_info],
-                ksp.getResidualNorm(),
-            )
-            LOGGER.warning(
-                'The convergence_history of length %s is %s',
-                len(ksp.getConvergenceHistory()),
-                ksp.getConvergenceHistory(),
-            )
-        petsc4py.PETSc.garbage_cleanup()
-        return problem.solution, info, ksp
+                LOGGER.warning(
+                    'The convergence_history of length %s is %s',
+                    len(ksp.getConvergenceHistory()),
+                    ksp.getConvergenceHistory(),
+                )
+            petsc4py.PETSc.garbage_cleanup()
+            return problem.solution, info, ksp
--- sostrades_core/execution_engine/gemseo_addon/linear_solvers/settings/petsc_ksp_settings.py
+++ sostrades_core/execution_engine/gemseo_addon/linear_solvers/settings/petsc_ksp_settings.py
@@ -15,58 +15,60 @@ limitations under the License.
 '''
 
 from __future__ import annotations
+from os import getenv
 
-from gemseo_petsc.linear_solvers.settings.petsc_ksp_settings import BasePetscKSPSettings
-from pydantic import Field, NonNegativeFloat
-from strenum import StrEnum
+if getenv("USE_PETSC", "").lower() in ("true", "1"):
+    from gemseo_petsc.linear_solvers.settings.petsc_ksp_settings import BasePetscKSPSettings
+    from pydantic import Field, NonNegativeFloat
+    from strenum import StrEnum
 
-"""Settings for the SoSTrades wrapper of PETSc KSP linear solvers."""
-
-
-class SoSPreconditionerType(StrEnum):
-    """The type of the precondtioner.
-
-    See
-    [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html].
-    """
-
-    JACOBI = "jacobi"
-    ILU = "ilu"
-    GASM = "gasm"
+    """Settings for the SoSTrades wrapper of PETSc KSP linear solvers."""
 
 
-class BaseSoSPetscKSPSettings(BasePetscKSPSettings):
-    """The settings of the PETSc KSP algorithms.
+    class SoSPreconditionerType(StrEnum):
+        """The type of the precondtioner.
 
-    The default numerical parameters differ from gemseo_petsc.
-    `_TARGET_CLASS_NAME` will be overloaded for each algorithm.
-    """
+        See
+        [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html].
+        """
 
-    atol: NonNegativeFloat = Field(
-        default=1e-200,
-        description="""The absolute convergence tolerance.
+        JACOBI = "jacobi"
+        ILU = "ilu"
+        GASM = "gasm"
 
-Absolute tolerance of the (possibly preconditioned) residual norm.
-Algorithm stops if norm(b - A @ x) <= max(rtol*norm(b), atol).""",
-    )
 
-    dtol: NonNegativeFloat = Field(
-        default=1e50,
-        description="""The divergence tolerance.
+    class BaseSoSPetscKSPSettings(BasePetscKSPSettings):
+        """The settings of the PETSc KSP algorithms.
 
-The amount the (possibly preconditioned) residual norm can increase.""",
-    )
+        The default numerical parameters differ from gemseo_petsc.
+        `_TARGET_CLASS_NAME` will be overloaded for each algorithm.
+        """
 
-    preconditioner_type: SoSPreconditionerType | None = Field(
-        default=SoSPreconditionerType.ILU,
-        description="""The type of the precondtioner.
+        atol: NonNegativeFloat = Field(
+            default=1e-200,
+            description="""The absolute convergence tolerance.
+    
+    Absolute tolerance of the (possibly preconditioned) residual norm.
+    Algorithm stops if norm(b - A @ x) <= max(rtol*norm(b), atol).""",
+        )
 
-See [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html]""",
-    )
+        dtol: NonNegativeFloat = Field(
+            default=1e50,
+            description="""The divergence tolerance.
+    
+    The amount the (possibly preconditioned) residual norm can increase.""",
+        )
 
-    rtol: NonNegativeFloat = Field(
-        default=1e-200,
-        description="""The relative convergence tolerance.
+        preconditioner_type: SoSPreconditionerType | None = Field(
+            default=SoSPreconditionerType.ILU,
+            description="""The type of the precondtioner.
+    
+    See [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html]""",
+        )
 
-Relative decrease in the (possibly preconditioned) residual norm.""",
-    )
+        rtol: NonNegativeFloat = Field(
+            default=1e-200,
+            description="""The relative convergence tolerance.
+    
+    Relative decrease in the (possibly preconditioned) residual norm.""",
+        )
