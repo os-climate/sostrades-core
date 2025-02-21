b16cf341fc108b83884e9189a7a825408105874b
magueylard <marie-audrey.gueylard@capgemini.com>
2025-02-21 15:08:54 +0100

    try to see petsc error again

--- sostrades_core/execution_engine/gemseo_addon/linear_solvers/settings/petsc_ksp_settings.py
+++ sostrades_core/execution_engine/gemseo_addon/linear_solvers/settings/petsc_ksp_settings.py
@@ -16,60 +16,58 @@ limitations under the License.
 
 from __future__ import annotations
 
-from os import getenv
+#if getenv("USE_PETSC", "").lower() in ("true", "1"):
+from gemseo_petsc.linear_solvers.settings.petsc_ksp_settings import BasePetscKSPSettings
+from pydantic import Field, NonNegativeFloat
+from strenum import StrEnum
 
-if getenv("USE_PETSC", "").lower() in ("true", "1"):
-    from gemseo_petsc.linear_solvers.settings.petsc_ksp_settings import BasePetscKSPSettings
-    from pydantic import Field, NonNegativeFloat
-    from strenum import StrEnum
-
-    """Settings for the SoSTrades wrapper of PETSc KSP linear solvers."""
+"""Settings for the SoSTrades wrapper of PETSc KSP linear solvers."""
 
 
-    class SoSPreconditionerType(StrEnum):
-        """The type of the precondtioner.
+class SoSPreconditionerType(StrEnum):
+    """The type of the precondtioner.
 
-        See
-        [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html].
-        """
+    See
+    [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html].
+    """
 
-        JACOBI = "jacobi"
-        ILU = "ilu"
-        GASM = "gasm"
+    JACOBI = "jacobi"
+    ILU = "ilu"
+    GASM = "gasm"
 
 
-    class BaseSoSPetscKSPSettings(BasePetscKSPSettings):
-        """The settings of the PETSc KSP algorithms.
+class BaseSoSPetscKSPSettings(BasePetscKSPSettings):
+    """The settings of the PETSc KSP algorithms.
 
-        The default numerical parameters differ from gemseo_petsc.
-        `_TARGET_CLASS_NAME` will be overloaded for each algorithm.
-        """
+    The default numerical parameters differ from gemseo_petsc.
+    `_TARGET_CLASS_NAME` will be overloaded for each algorithm.
+    """
 
-        atol: NonNegativeFloat = Field(
-            default=1e-200,
-            description="""The absolute convergence tolerance.
+    atol: NonNegativeFloat = Field(
+        default=1e-200,
+        description="""The absolute convergence tolerance.
 
-    Absolute tolerance of the (possibly preconditioned) residual norm.
-    Algorithm stops if norm(b - A @ x) <= max(rtol*norm(b), atol).""",
-        )
+Absolute tolerance of the (possibly preconditioned) residual norm.
+Algorithm stops if norm(b - A @ x) <= max(rtol*norm(b), atol).""",
+    )
 
-        dtol: NonNegativeFloat = Field(
-            default=1e50,
-            description="""The divergence tolerance.
+    dtol: NonNegativeFloat = Field(
+        default=1e50,
+        description="""The divergence tolerance.
 
-    The amount the (possibly preconditioned) residual norm can increase.""",
-        )
+The amount the (possibly preconditioned) residual norm can increase.""",
+    )
 
-        preconditioner_type: SoSPreconditionerType | None = Field(
-            default=SoSPreconditionerType.ILU,
-            description="""The type of the precondtioner.
+    preconditioner_type: SoSPreconditionerType | None = Field(
+        default=SoSPreconditionerType.ILU,
+        description="""The type of the precondtioner.
 
-    See [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html]""",
-        )
+See [https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html]""",
+    )
 
-        rtol: NonNegativeFloat = Field(
-            default=1e-200,
-            description="""The relative convergence tolerance.
+    rtol: NonNegativeFloat = Field(
+        default=1e-200,
+        description="""The relative convergence tolerance.
 
-    Relative decrease in the (possibly preconditioned) residual norm.""",
-        )
+Relative decrease in the (possibly preconditioned) residual norm.""",
+    )
