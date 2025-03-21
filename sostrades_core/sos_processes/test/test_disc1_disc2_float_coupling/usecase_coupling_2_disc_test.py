faa85792d2e7bd30cb4bdb1900aeef2d322729d6
magueylard <marie-audrey.gueylard@capgemini.com>
2025-03-21 16:32:16 +0100

    fix header and test

--- sostrades_core/sos_processes/test/test_disc1_disc2_float_coupling/usecase_coupling_2_disc_test.py
+++ sostrades_core/sos_processes/test/test_disc1_disc2_float_coupling/usecase_coupling_2_disc_test.py
@@ -1,6 +1,5 @@
 '''
-Copyright 2022 Airbus SAS
-Modifications on 2024/05/16-2024/06/28 Copyright 2024 Capgemini
+Copyright 2025 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
