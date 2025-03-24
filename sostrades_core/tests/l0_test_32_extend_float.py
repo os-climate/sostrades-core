fc79b701251c0ace6da8c8bd0b8d627f3682ae39
magueylard <marie-audrey.gueylard@capgemini.com>
2025-03-24 14:02:59 +0100

    add log in assert

--- sostrades_core/tests/l0_test_32_extend_float.py
+++ sostrades_core/tests/l0_test_32_extend_float.py
@@ -129,7 +129,7 @@ class TestExtendFloat(unittest.TestCase):
         self.assertTrue(isinstance(
             reconverted_data_dm['study.x'], type(x)))
         self.assertTrue(isinstance(
-            reconverted_data_dm['study.a'], np_int32))
+            reconverted_data_dm['study.a'], np_int32), f"the type of study.a is {type(reconverted_data_dm['study.a'])} instead of {type(a)}")
         self.assertEqual(reconverted_data_dm['study.y'], reconverted_data_dm['study.a'] * reconverted_data_dm['study.x'] + reconverted_data_dm['study.Disc1.b'])
         self.assertTrue(isinstance(
             reconverted_data_dm['study.y'], type(y)))
