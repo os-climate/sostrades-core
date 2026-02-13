93655dbbba60297b2c34a61583d1e08a47f96a70
Valentin Joncquieres <valentin.joncquieres@capgemini.com>
2026-02-13 10:12:54 +0100

    fix treeview namespace

--- sostrades_core/tools/tree/treeview.py
+++ sostrades_core/tools/tree/treeview.py
@@ -125,7 +125,7 @@ class TreeView:
         # the treenodes
         for namespace in ns_manager.ee.post_processing_manager.namespace_post_processing:
             try:
-                ns_list = [ns.get_value() for ns in ns_manager.get_all_namespace_with_name(namespace) if not ns_manager.unused_namespace(post_proc_ns)]
+                ns_list = [ns.get_value() for ns in ns_manager.get_all_namespace_with_name(namespace) if not ns_manager.unused_namespace(ns,post_proc_ns)]
                 for ns_value in ns_list:
                     if ns_value not in treenodes.keys():
                         treenode = self.add_treenode(
